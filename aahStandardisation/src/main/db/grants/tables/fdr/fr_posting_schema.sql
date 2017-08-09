57ba4c9c1b15d9c679a348281e3a1a3be85519b1
Kieft <ekieft@agl.com>
2017-08-09 16:45:11 -0400

    GL Combo Edit STN Files

---
+++ aahStandardisation/src/main/db/grants/tables/fdr/fr_posting_schema.sql
@@ -0,0 +1,1 @@
+grant select on fdr.fr_posting_schema to stn;
---
+++ aahStandardisation/src/main/db/views/stn/hopper_gl_combo_edit_gc.sql
@@ -0,0 +1,19 @@
+create or replace view stn.hopper_gl_combo_edit_gc
+as
+select
+       fsrgc.srgc_gct_code_type_id combo_set_cd
+     , fsrgc.srgc_client_code      combo_set_value
+     , fsrgc.srgc_valid_from       valid_from
+     , fsrgc.srgc_valid_to         valid_to
+     , fsrgc.srgc_active           combo_edit_sts
+     , fsrgc.event_status          event_status
+     , fsrgc.message_id            message_id
+     , fsrgc.process_id            process_id
+     , fsrgc.lpg_id                lpg_id
+  from
+       fdr.fr_stan_raw_general_codes fsrgc
+ where
+       fsrgc.srgc_gct_code_type_id like 'COMBO%'
+  with
+       check option
+     ;
