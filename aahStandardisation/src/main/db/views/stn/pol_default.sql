254cfe00258ba34ad2dee78bd490a00cce23f284
Hall, Andrew <AHall@agltd.com>
2017-08-24 17:34:35 -0400

    added new default values to support policy standardisation

--- aahStandardisation/src/main/db/views/stn/pol_default.sql
+++ aahStandardisation/src/main/db/views/stn/pol_default.sql
@@ -4,6 +4,12 @@ select
        max ( case when fgl.lk_match_key1 = 'SYSTEM_INSTANCE'
                   then fgl.lk_lookup_value1
              end )                                               system_instance
+     , max ( case when fgl.lk_match_key1 = 'RATE_TYPE_PREFIX'
+                  then fgl.lk_lookup_value1
+             end )                                               rate_type_prefix
+     , max ( case when fgl.lk_match_key1 = 'PARTY_LEGAL'
+                  then fgl.lk_lookup_value1
+             end )                                               party_legal
   from
        fdr.fr_general_lookup fgl
  where
