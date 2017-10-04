e505b4363cb2da481fbd0dc6d4c0478768769871
Kieft, Eric <EKieft@agltd.com>
2017-10-04 17:51:46 -0400

    seed data fixes

--- aahStandardisation/src/main/db/data/stn/posting_accounting_basis_type.sql
+++ aahStandardisation/src/main/db/data/stn/posting_accounting_basis_type.sql
@@ -1,3 +1,6 @@
 insert into stn.posting_accounting_basis_type ( basis_typ ) values ( 'STAT' );
 insert into stn.posting_accounting_basis_type ( basis_typ ) values ( 'GAAP' );
+insert into stn.posting_accounting_basis_type ( basis_typ ) values ( 'STAT_CONSOLIDATION' );
+insert into stn.posting_accounting_basis_type ( basis_typ ) values ( 'GAAP_CONSOLIDATION' );
+insert into stn.posting_accounting_basis_type ( basis_typ ) values ( 'RPT_ALIAS' );
 commit;
