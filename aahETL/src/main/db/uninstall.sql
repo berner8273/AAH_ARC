3ba48524ffe8c37bac4a7ca0896854ff60602241
Hall <ahall@agl.com>
2017-10-31 14:28:20 +0000

    Add scripts to create an Oracle user to support ETL delivery of data to AAH

---
+++ aahETL/src/main/db/uninstall.sql
@@ -0,0 +1,23 @@
+-- -----------------------------------------------------------------------------------------
+-- filename: install.sql
+-- author  : andrew hall
+-- purpose : Script to uninstall the users and roles which will be used AG's ETL processes.
+--         :
+-- -----------------------------------------------------------------------------------------
+
+whenever sqlerror exit failure
+
+set serveroutput on
+set define ~
+
+define tns_alias    = @oracleTnsAlias@
+define sys_user     = @sysUsername@
+define sys_password = @sysPassword@
+define sys_logon    = ~sys_user/~sys_password@~tns_alias
+
+conn ~sys_logon
+
+drop role aah_load;
+drop user aah_ssis;
+
+exit
