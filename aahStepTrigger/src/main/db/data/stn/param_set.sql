fd310e9c29cdc2d7aaae404e057948d93de2e65e
Kieft, Eric <EKieft@agltd.com>
2017-11-03 00:22:18 -0500

    slr steptrigger changes

--- aahStepTrigger/src/main/db/data/stn/param_set.sql
+++ aahStepTrigger/src/main/db/data/stn/param_set.sql
@@ -3,4 +3,5 @@ insert into stn.param_set ( param_set_id , param_set_cd ) values ( 2 , 'dsr-refe
 insert into stn.param_set ( param_set_id , param_set_cd ) values ( 3 , 'standardisation-transaction-data' );
 insert into stn.param_set ( param_set_id , param_set_cd ) values ( 4 , 'dsr-transaction-data' );
 insert into stn.param_set ( param_set_id , param_set_cd ) values ( 5 , 'utilities' );
+insert into stn.param_set ( param_set_id , param_set_cd ) values ( 6 , 'subledger' );
 commit;
