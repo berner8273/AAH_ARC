d3d6a9817052e7176971ca5aed24b7461022cfde
Kieft, Eric <EKieft@agltd.com>
2017-11-08 14:55:08 -0500

    execution type changes

--- aahStandardisation/src/main/db/data/stn/execution_type.sql
+++ aahStandardisation/src/main/db/data/stn/execution_type.sql
@@ -1,4 +1,4 @@
-insert into stn.execution_type ( execution_typ , execution_typ_descr ) values ( 'MTM'     , 'Mark to market' );
-insert into stn.execution_type ( execution_typ , execution_typ_descr ) values ( 'NON-MTM' , 'Non mark to market' );
-insert into stn.execution_type ( execution_typ , execution_typ_descr ) values ( 'FP'      , 'FP description' );
+insert into stn.execution_type ( execution_typ , execution_typ_descr ) values ( 'MTM'         , 'Mark to market' );
+insert into stn.execution_type ( execution_typ , execution_typ_descr ) values ( 'NON_MTM'     , 'Non mark to market' );
+insert into stn.execution_type ( execution_typ , execution_typ_descr ) values ( 'NON_MTM_DRV' , 'Non mark to market derivative' );
 commit;
