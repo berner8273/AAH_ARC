e95e22e98c988632d6cfda85fa3a2f27e401776b
Kieft, Eric <EKieft@agltd.com>
2017-11-17 11:29:31 -0600

    rdr view changes

--- aahRDR/src/main/db/views/rdr/rrv_accounting_event.sql
+++ aahRDR/src/main/db/views/rdr/rrv_accounting_event.sql
@@ -1,101 +1,111 @@
 create or replace view rdr.rrv_accounting_event
 as
 select
- ae_accevent_id            
-,ae_gl_account    as sub_acccount         
-,ae_acc_event_id           
-,ae_aet_acc_event_type_id  
-,ae_in_repository_ind      
-,ae_posting_date    as effective_date       
-,ae_accevent_date          
-,ae_amount            as transaction_amt     
-,ae_iso_currency_code as currency
-,ae_ledger_period          
-,ae_local_amount           
-,ae_gl_account_alias       
-,ae_local_currency_code    
-,ae_gl_entity        as business_unit      
-,ae_epg_id                 
-,ae_gl_book          as department
-,ae_gl_profit_centre       
-,ae_input_time             
-,ae_gl_instr_super_class   
-,ae_ledger_rec_status      
-,ae_value_date  		  as posting_period           
-,ae_gl_instrument_id       
-,ae_gl_party_business_id   
-,ae_gl_person_id           
-,ae_client_spare_id1 as event_type       
-,ae_client_spare_id2 as affiliate       
-,ae_client_spare_id3 as rule_id       
-,ae_client_spare_id4        
-,ae_client_spare_id5 as functionl_ccy      
-,ae_client_spare_id6 as functional_amt       
-,ae_client_spare_id7 as reporting_ccy       
-,ae_client_spare_id8 as reporting_amt       
-,ae_client_spare_id9       
-,ae_source_tran_no         
-,ae_gl_narrative           
-,ae_ledger_rec_status2     
-,ae_rep_schema_upd         
-,ae_transaction_no         
-,ae_rule_id                
-,ae_rules_amount_ref_id    
-,ae_sub_ledger_upd         
-,ae_i_instrument_id        
-,ae_fdr_tran_no            
-,ae_il_instr_leg_id        
-,ae_source_system          
-,ae_journal_type           
-,ae_gl_ledger_id           
-,ae_base_currency_code     
-,lpg_id                    
-,ae_source_jrnl_id         
-,ae_sub_event_id           
-,ae_posting_schema as ledger
-,ae_gaap           as accounting_basis         
-,ae_posting_code           
-,ae_reverse_date           
-,ae_dr_cr                  
-,ae_base_rate              
-,ae_local_rate             
-,ae_dimension_1  as affiliate          
-,ae_dimension_2  as chartfield1          
-,ae_dimension_3  as execution_type          
-,ae_dimension_4  as business_type
-,ae_dimension_5  as policy_id            
-,ae_dimension_7  as tax_jurisdiction
-,ae_dimension_8  as premium_type     
-,ae_dimension_10 as stream
-,ae_dimension_12 as gross_stream_owner           
-,ae_dimension_13 as underwriting_year      
-,ae_dimension_14 as owner_entity     
-,ae_dimension_15 as accounting_event_type
-,ae_gl_cost_centre         
-,ae_ret_amort_flag         
-,ae_gl_client1_org_unit_id 
-,ae_gl_client2_org_unit_id 
-,ae_gl_client3_org_unit_id 
-,ae_gl_plant_id            
-,ae_gl_ship_to_country_id  
-,ae_gl_product_part_id     
-,ae_client_date1           
-,ae_gl_cash_flow_type      
-,ae_gl_tax_code_id         
-,ae_gl_contract_id         
-,ae_gl_rights_categ_id     
-,ae_gl_rights_subcateg_id  
-,ae_translation_date       
-,ae_ret_recog_type_id      
-,ae_base_amount            
-,ae_number_of_periods      
-,ae_calc_period            
-,ae_ret_agv_or_arrears     
-,ae_ret_ca_calendar_name   
-,ae_ret_post_period        
-,ae_client_date2           
-,ae_client_date3           
-,ae_client_date4           
-,ae_client_date5           
-from fdr.fr_accounting_event
-;
+       ae_accevent_id
+     , ae_gl_account                sub_acccount
+     , ae_acc_event_id
+     , ae_aet_acc_event_type_id
+     , ae_in_repository_ind
+     , ae_posting_date              effective_date
+     , ae_accevent_date
+     , ae_amount                    transaction_amt
+     , ae_iso_currency_code         currency
+     , ae_ledger_period
+     , ae_local_amount
+     , ae_gl_account_alias
+     , ae_local_currency_code
+     , ae_gl_entity                 business_unit
+     , ae_epg_id
+     , ae_gl_book                   department
+     , ae_gl_profit_centre
+     , ae_input_time
+     , ae_gl_instr_super_class
+     , ae_ledger_rec_status
+     , ae_value_date                posting_period
+     , ae_gl_instrument_id
+     , ae_gl_party_business_id
+     , ae_gl_person_id
+     , ae_client_spare_id1
+     , ae_client_spare_id2
+     , ae_client_spare_id3          ultimate_parent_stream_id
+     , ae_client_spare_id4          event_typ
+     , ae_client_spare_id5          functionl_ccy
+     , ae_client_spare_id6          functional_amt
+     , ae_client_spare_id7          reporting_ccy
+     , ae_client_spare_id8          reporting_amt
+     , ae_client_spare_id9          is_mark_to_market
+     , ae_client_spare_id10         vie_cd
+     , ae_client_spare_id11         business_event_typ
+     , ae_client_spare_id12         event_seq_id
+     , ae_client_spare_id13         policy_typ
+     , ae_client_spare_id14         correlation_uuid
+     , ae_client_spare_id15         basis_cd
+     , ae_source_tran_no
+     , ae_gl_narrative
+     , ae_ledger_rec_status2
+     , ae_rep_schema_upd
+     , ae_transaction_no
+     , ae_rule_id
+     , ae_rules_amount_ref_id
+     , ae_sub_ledger_upd
+     , ae_i_instrument_id
+     , ae_fdr_tran_no
+     , ae_il_instr_leg_id
+     , ae_source_system
+     , ae_journal_type
+     , ae_gl_ledger_id
+     , ae_base_currency_code
+     , lpg_id
+     , ae_source_jrnl_id
+     , ae_sub_event_id
+     , ae_posting_schema            ledger
+     , ae_gaap                      accounting_basis
+     , ae_posting_code
+     , ae_reverse_date
+     , ae_dr_cr
+     , ae_base_rate
+     , ae_local_rate
+     , ae_dimension_1               chartfield_1
+     , ae_dimension_2               dept_cd
+     , ae_dimension_3               counterparty_le_cd
+     , ae_dimension_4               affiliate_le_cd
+     , ae_dimension_5               accident_yr
+     , ae_dimension_6               underwriting_yr
+     , ae_dimension_7               policy_id
+     , ae_dimension_8               stream_id
+     , ae_dimension_9               tax_jurisdiction_cd
+     , ae_dimension_10              ledger_cd
+     , ae_dimension_11              execution_typ
+     , ae_dimension_12              business_typ
+     , ae_dimension_13              owner_le_id
+     , ae_dimension_14              premium_typ
+     , ae_dimension_15              journal_descr
+     , ae_gl_cost_centre
+     , ae_ret_amort_flag
+     , ae_gl_client1_org_unit_id
+     , ae_gl_client2_org_unit_id
+     , ae_gl_client3_org_unit_id
+     , ae_gl_plant_id
+     , ae_gl_ship_to_country_id
+     , ae_gl_product_part_id
+     , ae_client_date1
+     , ae_gl_cash_flow_type
+     , ae_gl_tax_code_id
+     , ae_gl_contract_id
+     , ae_gl_rights_categ_id
+     , ae_gl_rights_subcateg_id
+     , ae_translation_date
+     , ae_ret_recog_type_id
+     , ae_base_amount
+     , ae_number_of_periods
+     , ae_calc_period
+     , ae_ret_agv_or_arrears
+     , ae_ret_ca_calendar_name
+     , ae_ret_post_period
+     , ae_client_date2
+     , ae_client_date3
+     , ae_client_date4
+     , ae_client_date5
+  from
+       fdr.fr_accounting_event
+     ;
--- aahRDR/src/main/db/views/rdr/rrv_stan_raw_acc_event.sql
+++ aahRDR/src/main/db/views/rdr/rrv_stan_raw_acc_event.sql
@@ -1,74 +1,74 @@
 create or replace view rdr.rrv_stan_raw_acc_event
 as
 select
-        srae_raw_acc_event_id        
-     ,  srae_acc_event_type          event_type
-     ,  srae_sub_event_id            
-     ,  srae_acc_event_id            
-     ,  srae_accevent_date           accounting_dt
-     ,  srae_dimension_3             program    
-     ,  srae_dimension_4             affiliate    
-     ,  srae_dimension_5             accident_year
-     ,  srae_dimension_6             underwriting_year
-     ,  srae_dimension_7             policy_id
-     ,  srae_dimension_8             stream_id
-     ,  srae_dimension_9             tax_jurisdiction
-     ,  srae_dimension_10            ledger
-     ,  srae_dimension_11            execution_type
-     ,  srae_dimension_12            business_type
-     ,  srae_dimension_13            owner_entity
-     ,  srae_dimension_14            premium_type
-     ,  srae_dimension_15            journal_descr
-     ,  srae_gl_book                 
-     ,  srae_gl_entity               business_unit             
-     ,  srae_instr_super_class       
-     ,  srae_instrument_code         
-     ,  srae_gl_party_business_code  
-     ,  srae_gl_person_code          
-     ,  srae_iso_currency_code       transaction_ccy
-     ,  srae_client_amount1          transaction_amt
-     ,  srae_client_spare_id2        issue_type
-     ,  srae_client_spare_id3        ultimate_parent_le_id
-     ,  srae_client_spare_id5        functional_ccy
-     ,  srae_client_spare_id6        functional_amt
-     ,  srae_client_spare_id7        reporting_ccy
-     ,  srae_client_spare_id8        reporting_amt
-     ,  srae_client_spare_id9        is_mark_to_market
-     ,  srae_client_spare_id10       vie_cd
-     ,  srae_client_spare_id11       business_event_type
-     ,  srae_client_spare_id1        
-     ,  srae_il_instr_leg_id         
-     ,  srae_posting_date            
-     ,  srae_source_system           
-     ,  srae_source_tran_no          
-     ,  srae_value_date              
-     ,  srae_nostro_account        account_lookup
-     ,  srae_pos_neg_flag            
-     ,  srae_input_by                
-     ,  srae_input_time              
-     ,  srae_one                     
-     ,  arrival_time                 
-     ,  event_error_string           
-     ,  event_status                 
-     ,  lpg_id                       
-     ,  srae_party_sys_inst_code     
-     ,  srae_static_sys_inst_code    
-     ,  srae_instr_sys_inst_code     
-     ,  srae_client_spare_id12       
-     ,  srae_client_spare_id13       
-     ,  srae_client_spare_id14       
-     ,  srae_client_spare_id15       
-     ,  srae_client_spare_id16       
-     ,  srae_client_spare_id17       
-     ,  srae_client_spare_id18       
-     ,  srae_client_spare_id19       
-     ,  srae_client_spare_id20       
-     ,  srae_client_date1            
-     ,  srae_client_date2            
-     ,  srae_client_date3            
-     ,  srae_client_date4            
-     ,  srae_client_date5            
+       srae_raw_acc_event_id
+     , srae_acc_event_type          event_type
+     , srae_sub_event_id
+     , srae_acc_event_id
+     , srae_accevent_date           accounting_dt
+     , srae_dimension_1             chartfield_1
+     , srae_dimension_2             dept_cd
+     , srae_dimension_3             counterparty_le_cd
+     , srae_dimension_4             affiliate_le_cd
+     , srae_dimension_5             accident_yr
+     , srae_dimension_6             underwriting_yr
+     , srae_dimension_7             policy_id
+     , srae_dimension_8             stream_id
+     , srae_dimension_9             tax_jurisdiction_cd
+     , srae_dimension_10            ledger_cd
+     , srae_dimension_11            execution_typ
+     , srae_dimension_12            business_typ
+     , srae_dimension_13            owner_le_id
+     , srae_dimension_14            premium_typ
+     , srae_dimension_15            journal_descr
+     , srae_gl_book
+     , srae_gl_entity               business_unit
+     , srae_instr_super_class
+     , srae_instrument_code
+     , srae_gl_party_business_code
+     , srae_gl_person_code
+     , srae_iso_currency_code       transaction_ccy
+     , srae_client_amount1          transaction_amt
+     , srae_client_spare_id3        ultimate_parent_stream_id
+     , srae_client_spare_id4        event_typ
+     , srae_client_spare_id5        functionl_ccy
+     , srae_client_spare_id6        functional_amt
+     , srae_client_spare_id7        reporting_ccy
+     , srae_client_spare_id8        reporting_amt
+     , srae_client_spare_id9        is_mark_to_market
+     , srae_client_spare_id10       vie_cd
+     , srae_client_spare_id11       business_event_typ
+     , srae_client_spare_id12       event_seq_id
+     , srae_client_spare_id13       policy_typ
+     , srae_client_spare_id14       correlation_uuid
+     , srae_client_spare_id15       basis_cd
+     , srae_il_instr_leg_id
+     , srae_posting_date
+     , srae_source_system
+     , srae_source_tran_no
+     , srae_value_date
+     , srae_nostro_account          account_lookup
+     , srae_pos_neg_flag
+     , srae_input_by
+     , srae_input_time
+     , srae_one
+     , arrival_time
+     , event_error_string
+     , event_status
+     , lpg_id
+     , srae_party_sys_inst_code
+     , srae_static_sys_inst_code
+     , srae_instr_sys_inst_code
+     , srae_client_spare_id16
+     , srae_client_spare_id17
+     , srae_client_spare_id18
+     , srae_client_spare_id19
+     , srae_client_spare_id20
+     , srae_client_date1
+     , srae_client_date2
+     , srae_client_date3
+     , srae_client_date4
+     , srae_client_date5
   from
        fdr.fr_stan_raw_acc_event
-;
-
+     ;
