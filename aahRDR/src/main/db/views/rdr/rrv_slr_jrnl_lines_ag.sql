ef3b96354b7af8bfa26156634325f87a70dff0ef
Andrew Hall <ahall@agl.com>
2017-11-17 10:20:54 +0000

    corrected column naming

--- aahRDR/src/main/db/views/rdr/rrv_slr_jrnl_lines_ag.sql
+++ aahRDR/src/main/db/views/rdr/rrv_slr_jrnl_lines_ag.sql
@@ -10,44 +10,38 @@ select
      , jl_jrnl_process_id
      , jl_description
      , jl_source_jrnl_id
-     , jl_effective_date
-     , jl_value_date
-     , jl_entity               business_unit
+     , jl_effective_date   effective_date
+     , jl_value_date       posting_period
+     , jl_entity           business_unit
      , jl_epg_id
-     , jl_account              sub_account
-     , jl_segment_1            ledger
-     , jl_segment_2            accounting_basis
-     , jl_segment_3            department
-     , jl_segment_4            execution_type
-     , jl_segment_5            premium_type
-     , jl_segment_6            program
-     , jl_segment_7            chartfield1
-     , jl_segment_8            business_type
-     , jl_segment_9            policy_policysub
-     , jl_segment_10           line_of_business
-     , jl_attribute_1          int_counterparty
-     , jl_attribute_2          ext_counterparty
-     , jl_attribute_3          stream
-     , jl_attribute_4          issue_type
-     , jl_attribute_5          tax_jurisdiction
-     , jl_reference_1          underwriting_yr
-     , jl_reference_2          accident_yr
-     , jl_reference_3          policy_name
-     , jl_reference_4          ultimate_parent
-     , jl_reference_5          event_type
-     , jl_reference_6          affiliate
-     , jl_reference_7          rule_id
-     , jl_reference_8
-     , jl_reference_9
-     , jl_reference_10
-     , jl_tran_ccy
-     , jl_tran_amount
-     , jl_base_rate
-     , jl_base_ccy
-     , jl_base_amount
-     , jl_local_rate
-     , jl_local_ccy
-     , jl_local_amount
+     , jl_account          sub_account
+     , jl_segment_1        ledger
+     , jl_segment_2        accounting_basis
+     , jl_segment_3        department
+     , jl_segment_4        affiliate
+     , jl_segment_5        chartfield1
+     , jl_segment_6        execution_type
+     , jl_segment_7        business_type
+     , jl_segment_8        policy_id
+     , jl_attribute_1      stream
+     , jl_attribute_2      tax_jurisdiction
+     , jl_attribute_3      premium_type
+     , jl_attribute_4      accounting_event_type
+     , jl_reference_1      journal_descr
+     , jl_reference_2      gross_stream_owner
+     , jl_reference_3      underwriting_year
+     , jl_reference_4      owner_entity
+     , jl_reference_5      business_event_type
+     , jl_reference_6      accident_year
+     , jl_reference_7      int_ext_counterparty
+     , jl_tran_ccy         currency
+     , jl_tran_amount      transaction_amt
+     , jl_base_rate        reporting_rate
+     , jl_base_ccy         reporting_currency
+     , jl_base_amount      reporting_amt
+     , jl_local_rate       functional_rate
+     , jl_local_ccy        functional_currency
+     , jl_local_amount     functional_amt
      , jl_created_by
      , jl_created_on
      , jl_amended_by
@@ -61,4 +55,4 @@ select
      , jl_type
   from
        slr.slr_jrnl_lines
-;
+     ;
