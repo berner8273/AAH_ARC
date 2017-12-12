a2145b74a13eb2ef79e4f784460f32cfbc7f72f6
Kieft, Eric <EKieft@agltd.com>
2017-12-11 22:50:42 -0500

    cession event changes

---
+++ aahStandardisation/src/main/db/grants/tables/fdr/fr_accounting_event_imp.sql
@@ -0,0 +1,1 @@
+grant select on fdr.fr_accounting_event_imp to stn;
---
+++ aahStandardisation/src/main/db/tables/stn/business_event.sql
@@ -0,0 +1,5 @@
+create table stn.business_event
+(
+    business_event_cd           varchar2 ( 50 char ) not null
+,   constraint pk_be            primary key ( business_event_cd )
+);
