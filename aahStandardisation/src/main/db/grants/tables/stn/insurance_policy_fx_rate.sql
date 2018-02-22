0f92a5358f4b930be68ed1f2ac8f351291ea5cd2
jberner <jberner@assuredguaranty.com>
2018-02-22 14:26:51 -0500

    1-3-0 patch fixes for resubmit and combo edit

---
+++ aahStandardisation/src/main/db/grants/tables/stn/insurance_policy_fx_rate.sql
@@ -0,0 +1,3 @@
+grant update on stn.insurance_policy_fx_rate to gui;
+grant select on stn.insurance_policy_fx_rate to gui;
+
---
+++ aahStandardisation/src/main/db/grants/tables/stn/insurance_policy_tax_jurisd.sql
@@ -0,0 +1,2 @@
+grant update on stn.insurance_policy_tax_jurisd to gui;
+grant select on stn.insurance_policy_tax_jurisd to gui;
