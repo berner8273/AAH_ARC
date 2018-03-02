b6692ac4d9f74f35462a93e1054ac57ab91549f1
Kieft, Eric <EKieft@agltd.com>
2018-03-02 07:43:22 -0600

    view change

--- aahRDR/src/main/db/views/rdr/rrv_ag_event_class_period.sql
+++ aahRDR/src/main/db/views/rdr/rrv_ag_event_class_period.sql
@@ -1,11 +1,38 @@
-create or replace view rdr.rrv_ag_event_class_period
-as
+--create or replace view rdr.rrv_ag_event_class_period
+--as
 select
-  lk_match_key1		event_class 
-  ,lk_match_key2	business_year		
-  ,lk_match_key3	business_month	
-  ,lk_lookup_value1	closed_status
-  ,lk_lookup_value4	closed_date
-from fdr.fr_general_lookup
-where lk_lkt_lookup_type_code = 'EVENT_CLASS_PERIOD'
+       fgl.lk_match_key1                              event_class
+     , fgl.lk_match_key2                              business_year
+     , lpad(lk_match_key3,2,'0')                      business_month
+     , fgl.lk_match_key2||lpad(lk_match_key3,2,'0')   period
+     , fgl.lk_lookup_value1                           closed_status
+  from
+       fdr.fr_general_lookup fgl
+ where
+       fgl.lk_lkt_lookup_type_code = 'EVENT_CLASS_PERIOD'
+   and fgl.lk_lookup_value1        = 'C'
+union
+select
+       fgl.lk_match_key1                              event_class
+     , fgl.lk_match_key2                              business_year
+     , lpad(lk_match_key3,2,'0')                      business_month
+     , fgl.lk_match_key2||lpad(lk_match_key3,2,'0')   period
+     , fgl.lk_lookup_value1                           closed_status
+  from
+       fdr.fr_general_lookup fgl
+  join ( select
+                min(fgl2.lk_match_key2||fgl2.lk_match_key3)   min_period
+              , fgl2.lk_match_key1                            event_class
+           from
+                fdr.fr_general_lookup fgl2
+          where
+                fgl2.lk_lkt_lookup_type_code = 'EVENT_CLASS_PERIOD'
+            and fgl2.lk_lookup_value1        = 'O'
+          group by 
+                fgl2.lk_match_key1
+       ) open_period
+    on fgl.lk_match_key2||lk_match_key3 = open_period.min_period
+   and fgl.lk_match_key1                = open_period.event_class
+ where
+       fgl.lk_lkt_lookup_type_code = 'EVENT_CLASS_PERIOD'
 ;
