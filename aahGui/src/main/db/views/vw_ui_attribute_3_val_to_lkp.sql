db19c5b68bea8896b25e89607e22e5d9ba2d9a2c
Heavey, Anthony <AHeavey@assuredguaranty.com>
2018-03-13 18:45:34 -0400

    gui folder

--- aahGui/src/main/db/views/vw_ui_attribute_3_val_to_lkp.sql
+++
@@ -1,9 +0,0 @@
-create or replace view gui.vw_ui_attribute_3_val_to_lkp
-(
-   fdr_lookup_key,
-   description
-) as
-select premium_typ,
-       premium_typ
-from stn.journal_line_premium_type
-;
--- aahGui/src/main/db/views/vw_ui_attribute_4_val_to_lkp.sql
+++
@@ -1,9 +0,0 @@
-create or replace view gui.vw_ui_attribute_4_val_to_lkp
-(
-   fdr_lookup_key,
-   description
-) as
-select aet_acc_event_type_id,
-       aet_acc_event_type_name
-from fdr.fr_acc_event_type
-;
