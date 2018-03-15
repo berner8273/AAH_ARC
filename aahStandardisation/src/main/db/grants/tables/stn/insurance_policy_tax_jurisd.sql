6a258c0347077e08635d5490876e937af1a18482
jberner <jberner@assuredguaranty.com>
2018-03-15 09:38:35 -0400

    added grant option

--- aahStandardisation/src/main/db/grants/tables/stn/insurance_policy_tax_jurisd.sql
+++ aahStandardisation/src/main/db/grants/tables/stn/insurance_policy_tax_jurisd.sql
@@ -1,2 +1,2 @@
-grant update on stn.insurance_policy_tax_jurisd to gui;
-grant select on stn.insurance_policy_tax_jurisd to gui;
+grant update on stn.insurance_policy_tax_jurisd to gui with grant option;
+grant select on stn.insurance_policy_tax_jurisd to gui with grant option;
