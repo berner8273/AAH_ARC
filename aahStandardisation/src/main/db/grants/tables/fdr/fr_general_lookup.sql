251efb3e53e4a94f667527cc813db11954afe47f
jberner <jberner@assuredguaranty.com>
2018-03-15 08:58:17 -0400

    glookup grant

--- aahStandardisation/src/main/db/grants/tables/fdr/fr_general_lookup.sql
+++ aahStandardisation/src/main/db/grants/tables/fdr/fr_general_lookup.sql
@@ -1,3 +1,3 @@
 grant select on fdr.fr_general_lookup to stn;
 grant update on fdr.fr_general_lookup to stn;
-grant update on fdr.fr_general_lookup to gui with grant option;
+grant select on fdr.fr_general_lookup to gui with grant option;
