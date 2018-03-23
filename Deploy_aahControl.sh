c28dfe5e769dd01aa26810d9639da2eee799519e
Elli Admin <EWA@assuredguaranty.com>
2018-03-23 15:24:30 +0000

    Updated Deploy_aahControl.sh

--- Deploy_aahControl.sh
+++ Deploy_aahControl.sh
@@ -4,10 +4,10 @@
 # Info    : Octopus Deploy.sh script for aahControl package
 # Date    : 2018-01-19
 # Author  : Elli Wang
-# Version : 2018022801
+# Version : 2018032301
 # Note    :
-#   2018-02-28	Elli	GA 5.10.7.1
-#   2018-02-02	Elli	GA 5.10.7
+#   2018-03-09	Elli	GA 5.10.7
+#   2018-02-02	Elli	GA 1.2.2
 ###############################################################################
 # Variables
 PATH="/usr/bin"
@@ -21,7 +21,7 @@ DST_DIR="/aah/scripts"
 
 # Command variables
 INSTALL="/usr/bin/install"
-RM="/usr/bin/rm"
+FIND="/usr/bin/find"
 
 # Functions ===================================================================
 # Exit with an error
@@ -43,7 +43,8 @@ RUN () {
 printf "*** $PROGRAM starts ...\n"
 
 # Check if debug mode
-if [[ $(get_octopusvariable "AAH.Octopus.RunScripts"|tr '[A-Z]' '[a-z]') = "false" ]]; then
+if [[ $(get_octopusvariable "AAH.Octopus.RunScripts"|tr '[A-Z]' '[a-z]') \
+		= "false" ]]; then
 	printf "** Run scripts in debug mode!!!\n"
 	IS_DEBUG=1
 fi
@@ -52,15 +53,15 @@ fi
 [[ ! -w "$DST_DIR" ]] && ERR_EXIT "$DST_DIR is not writable!"
 
 # Delete files
-printf "* Delete files ...\n"
-ls $DST_DIR/* | while read file; do
-	RUN $RM -f $file || RC=2
-done
+printf "* Delete files from $DST_DIR ...\n"
+RUN $FIND $DST_DIR -xdev -mindepth 1 -print -delete \
+	|| ERR_EXIT "$Cannot remove files from $DST_DIR!"
 
 # Copy files
 printf "* Copy files ...\n"
 ls $SRC_DIR/* | while read file; do
-	RUN $INSTALL -m 750 -pv $file $DST_DIR/ || RC=2
+	RUN $INSTALL -m 750 -pv $file $DST_DIR/ \
+		|| ERR_EXIT "Cannot copy $file to $DST_DIR/!"
 done
 
 # End =========================================================================
