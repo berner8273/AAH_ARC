3191c07d2988a90104e2dc57726c10fabca6b8ea
Kieft, Eric <EKieft@agltd.com>
2018-05-08 10:29:30 -0500

    user changes

--- aahETL/src/main/db/uninstall.sql
+++ aahETL/src/main/db/uninstall.sql
@@ -27,6 +27,7 @@ begin
                         trim ( lower ( username ) ) in (
                                                          trim ( lower ( 'aah_ssis' ) )
                                                        , trim ( lower ( 'aah_read' ) )
+                                                       , trim ( lower ( 'aah_report' ) )
                                                        , trim ( lower ( 'aah_ps' ) )
                                                        )
              )
