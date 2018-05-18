e8e2196c37770446bcf3e83c28b54a422a870544
jberner <jberner@assuredguaranty.com>
2018-05-18 10:54:17 -0400

    journal line changes

---
+++ aahStandardisation/src/main/db/data/stn/journal_line_source_type.sql
@@ -0,0 +1,5 @@
+insert into stn.journal_line_source_type (SOURCE_TYP_CD, SOURCE_TYP_DESCR) values ('HL', 'Historical Load');
+insert into stn.journal_line_source_type (SOURCE_TYP_CD, SOURCE_TYP_DESCR) values ('TC', 'Trade Credit Business');
+insert into stn.journal_line_source_type (SOURCE_TYP_CD, SOURCE_TYP_DESCR) values ('CA', 'Cash');
+insert into stn.journal_line_source_type (SOURCE_TYP_CD, SOURCE_TYP_DESCR) values ('PC', 'Profit Commissions');
+commit;
---
+++ aahStandardisation/src/main/db/tables/stn/journal_line_source_type.sql
@@ -0,0 +1,5 @@
+create table stn.journal_line_source_type
+(
+    source_typ_cd 	 varchar2 (30 ) not null
+,   source_typ_descr varchar2 (100) not null
+);
