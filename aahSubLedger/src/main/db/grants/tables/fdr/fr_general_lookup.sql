2d4f438c3707fbab9b094ebd24ebd9078fdbcd10
Nulty, Thomas <TNulty@agltd.com>
2018-06-13 14:55:49 +0000

    Updated fr_general_lookup.sql

--- aahSubLedger/src/main/db/grants/tables/fdr/fr_general_lookup.sql
+++ aahSubLedger/src/main/db/grants/tables/fdr/fr_general_lookup.sql
@@ -1,3 +1,4 @@
 grant select on fdr.fr_general_lookup to slr with grant option;
 grant insert on fdr.fr_general_lookup to slr with grant option;
 grant update on fdr.fr_general_lookup to slr with grant option;
+grant update on fdr.fr_general_lookup to rdr with grant option;
