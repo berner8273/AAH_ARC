8e7135ab37bfe5672835f6bfee00221759936a3d
Nulty, Thomas <TNulty@agltd.com>
2018-06-20 14:30:35 +0000

    Updated rcv_glint_journal_line.sql

--- aahSubLedger/src/main/db/views/rdr/rcv_glint_journal_line.sql
+++ aahSubLedger/src/main/db/views/rdr/rcv_glint_journal_line.sql
@@ -1,49 +1,139 @@
-create or replace view rdr.rcv_glint_journal_line (jl_jrnl_hdr_id, jl_jrnl_line_number, jl_fak_id, jl_eba_id, jl_jrnl_status, jl_jrnl_status_text, jl_jrnl_process_id, jl_description, jl_source_jrnl_id, jl_effective_date, jl_value_date, jl_entity, jl_epg_id, jl_account, jl_sub_account, jl_segment_1, jl_segment_2, jl_segment_3, jl_segment_4, jl_segment_5, jl_segment_6, jl_segment_7, jl_segment_8, jl_segment_9, jl_segment_10, jl_attribute_1, jl_attribute_2, jl_attribute_3, jl_attribute_4, jl_attribute_5, jl_reference_1, jl_reference_2, jl_reference_3, jl_reference_4, jl_reference_5, jl_reference_6, jl_reference_7, jl_reference_8, jl_reference_9, jl_reference_10, jl_tran_ccy, jl_tran_amount, jl_base_rate, jl_base_ccy, jl_base_amount, jl_local_rate, jl_local_ccy, jl_local_amount, jl_created_by, jl_created_on, jl_amended_by, jl_amended_on, jl_recon_status, event_group, credit_amt, debit_amt, event_status) AS 
- select jl_jrnl_hdr_id,
-          cast(jl_jrnl_line_number as number(5,0)) as jl_jrnl_line_number,
+create or replace view rdr.rcv_glint_journal_line 
+(
+   JL_JRNL_HDR_ID,
+   JL_JRNL_LINE_NUMBER,
+   JL_FAK_ID,
+   JL_EBA_ID,
+   JL_JRNL_STATUS,
+   JL_JRNL_STATUS_TEXT,
+   JL_JRNL_PROCESS_ID,
+   JL_DESCRIPTION,
+   JL_SOURCE_JRNL_ID,
+   JL_EFFECTIVE_DATE,
+   JL_VALUE_DATE,
+   JL_ENTITY,
+   JL_EPG_ID,
+   JL_ACCOUNT,
+   JL_SUB_ACCOUNT,
+   JL_SEGMENT_1,
+   JL_SEGMENT_2,
+   JL_SEGMENT_3,
+   JL_SEGMENT_4,
+   JL_SEGMENT_5,
+   PROGRAM_CODE,
+   JL_SEGMENT_7,
+   JL_SEGMENT_8,
+   JL_SEGMENT_9,
+   JL_SEGMENT_10,
+   JL_ATTRIBUTE_1,
+   JL_ATTRIBUTE_2,
+   JL_ATTRIBUTE_3,
+   JL_ATTRIBUTE_4,
+   JL_ATTRIBUTE_5,
+   JL_REFERENCE_1,
+   JL_REFERENCE_2,
+   JL_REFERENCE_3,
+   JL_REFERENCE_4,
+   JL_REFERENCE_5,
+   JL_REFERENCE_6,
+   JL_REFERENCE_7,
+   JL_REFERENCE_8,
+   JL_REFERENCE_9,
+   JL_REFERENCE_10,
+   JL_TRAN_CCY,
+   JL_TRAN_AMOUNT,
+   JL_BASE_RATE,
+   JL_BASE_CCY,
+   JL_BASE_AMOUNT,
+   JL_LOCAL_RATE,
+   JL_LOCAL_CCY,
+   JL_LOCAL_AMOUNT,
+   JL_CREATED_BY,
+   JL_CREATED_ON,
+   JL_AMENDED_BY,
+   JL_AMENDED_ON,
+   JL_RECON_STATUS,
+   EVENT_CLASS,
+   CREDIT_AMT,
+   DEBIT_AMT,
+   EVENT_STATUS,
+   MANUAL_JE
+)
+AS
+   SELECT CASE WHEN jt.ejt_madj_flag = 'Y' THEN jl_jrnl_hdr_id ELSE 0 END as jl_jrnl_hdr_id,
+          CAST (jl_jrnl_line_number AS NUMBER (5, 0)) AS jl_jrnl_line_number,
           jl_fak_id,
           jl_eba_id,
           jl_jrnl_status,
           jl_jrnl_status_text,
-          cast(jl_jrnl_process_id as number(8,0)) as jl_jrnl_process_id,
-          cast(nvl(jl_description,' ') as varchar2(30)) as jl_description,
-          cast(jl_source_jrnl_id as varchar2(10)) as jl_source_jrnl_id,
+          CAST (jl_jrnl_process_id AS NUMBER (8, 0)) AS jl_jrnl_process_id,
+          CAST (NVL (jl_description, ' ') AS VARCHAR2 (30)) AS jl_description,
+          CAST (jl_source_jrnl_id AS VARCHAR2 (10)) AS jl_source_jrnl_id,
           jl_effective_date,
           jl_value_date,
-          cast(jl_entity as varchar2(5)) as jl_entity,
+          CAST (jl_entity AS VARCHAR2 (5)) AS jl_entity,
           jl_epg_id,
-          cast(gl.ga_client_text4 as varchar2(10)) as jl_account,
-          jl_account as jl_sub_account,
-          cast(case when jl_segment_1 = 'NVS' then ' ' else jl_segment_1 end as varchar2(10)) as jl_segment_1,
-          case when jl_segment_2 = 'NVS' then ' ' else jl_segment_2 end as jl_segment_2,
-          cast(case when jl_segment_3 = 'NVS' then ' ' else jl_segment_3 end as varchar2(10)) as jl_segment_3,
-          cast(case when jl_segment_4 = 'NVS' then ' ' else jl_segment_4 end as varchar2(5)) as jl_segment_4,
-          cast(case when jl_segment_5 = 'NVS' then ' ' else jl_segment_5 end as varchar2(6)) as jl_segment_5,
-          cast(case when jl_segment_6 = 'NVS' then ' ' else jl_segment_6 end as varchar2(5)) as jl_segment_6,
-          case when jl_segment_7 = 'NVS' then ' ' else jl_segment_7 end as jl_segment_7,
-          case when jl_segment_8 = 'NVS' then ' ' else jl_segment_8 end as jl_segment_8,
-          case when jl_segment_9 = 'NVS' then ' ' else jl_segment_9 end as jl_segment_9,
-          case when jl_segment_10 = 'NVS' then ' ' else jl_segment_10 end as jl_segment_10,
-          case when jl_attribute_1 = 'NVS' then ' ' else jl_attribute_1 end as jl_attribute_1,
-          case when jl_attribute_2 = 'NVS' then ' ' else jl_attribute_2 end as jl_attribute_2,
-          case when jl_attribute_3 = 'NVS' then ' ' else jl_attribute_3 end as jl_attribute_3,
-          case when jl_attribute_4 = 'NVS' then ' ' else jl_attribute_4 end as jl_attribute_4,
-          case when jl_attribute_5 = 'NVS' then ' ' else jl_attribute_5 end as jl_attribute_5,
-          case when jl_reference_1 = 'NVS' then ' ' else jl_reference_1 end as jl_reference_1,
-          case when jl_reference_2 = 'NVS' then ' ' else jl_reference_2 end as jl_reference_2,
-          case when jl_reference_3 = 'NVS' then ' ' else jl_reference_3 end as jl_reference_3,
-          case when jl_reference_4 = 'NVS' then ' ' else jl_reference_4 end as jl_reference_4,
-          case when jl_reference_5 = 'NVS' then ' ' else jl_reference_5 end as jl_reference_5,
-          case when jl_reference_6 = 'NVS' then ' ' else jl_reference_6 end as jl_reference_6,
-          case when jl_reference_7 = 'NVS' then ' ' else jl_reference_7 end as jl_reference_7,
-          case when jl_reference_8 = 'NVS' then ' ' else jl_reference_8 end as jl_reference_8,
-          case when jl_reference_9 = 'NVS' then ' ' else jl_reference_9 end as jl_reference_9,
-          case when jl_reference_10 = 'NVS' then ' ' else jl_reference_10 end as jl_reference_10,
-          cast(jl_tran_ccy as varchar2(3)) as jl_tran_ccy,
-          cast(jl_tran_amount as number(23,3)) as jl_tran_amount,
+          CAST (gl.ga_client_text4 AS VARCHAR2 (10)) AS jl_account,
+          jl_account AS jl_sub_account,
+          CAST (
+             CASE WHEN jl_segment_1 = 'NVS' THEN ' ' ELSE jl_segment_1 END AS VARCHAR2 (10))
+             AS jl_segment_1,
+          CASE WHEN jl_segment_2 = 'NVS' THEN ' ' ELSE jl_segment_2 END
+             AS jl_segment_2,
+          CAST (
+             CASE WHEN jl_segment_3 = 'NVS' THEN ' ' ELSE jl_segment_3 END AS VARCHAR2 (10))
+             AS jl_segment_3,
+          CAST (
+             CASE WHEN jl_segment_4 = 'NVS' THEN ' ' ELSE jl_segment_4 END AS VARCHAR2 (5))
+             AS jl_segment_4,
+          CAST (
+             CASE WHEN jl_segment_5 = 'NVS' THEN ' ' ELSE jl_segment_5 END AS VARCHAR2 (6))
+             AS jl_segment_5,
+          CASE WHEN jl_entity LIKE 'E%' THEN 'MNCON' ELSE ' ' END
+             AS program_code,
+          CASE WHEN jl_segment_7 = 'NVS' THEN ' ' ELSE jl_segment_7 END
+             AS jl_segment_7,
+          CASE WHEN jl_segment_8 = 'NVS' THEN ' ' ELSE jl_segment_8 END
+             AS jl_segment_8,
+          CASE WHEN jl_segment_9 = 'NVS' THEN ' ' ELSE jl_segment_9 END
+             AS jl_segment_9,
+          CASE WHEN jl_segment_10 = 'NVS' THEN ' ' ELSE jl_segment_10 END
+             AS jl_segment_10,
+          CASE WHEN jl_attribute_1 = 'NVS' THEN ' ' ELSE jl_attribute_1 END
+             AS jl_attribute_1,
+          CASE WHEN jl_attribute_2 = 'NVS' THEN ' ' ELSE jl_attribute_2 END
+             AS jl_attribute_2,
+          CASE WHEN jl_attribute_3 = 'NVS' THEN ' ' ELSE jl_attribute_3 END
+             AS jl_attribute_3,
+          CASE WHEN jl_attribute_4 = 'NVS' THEN ' ' ELSE jl_attribute_4 END
+             AS jl_attribute_4,
+          CASE WHEN jl_attribute_5 = 'NVS' THEN ' ' ELSE jl_attribute_5 END
+             AS jl_attribute_5,
+          CASE WHEN jl_reference_1 = 'NVS' THEN ' ' ELSE jl_reference_1 END
+             AS jl_reference_1,
+          CASE WHEN jl_reference_2 = 'NVS' THEN ' ' ELSE jl_reference_2 END
+             AS jl_reference_2,
+          CASE WHEN jl_reference_3 = 'NVS' THEN ' ' ELSE jl_reference_3 END
+             AS jl_reference_3,
+          CASE WHEN jl_reference_4 = 'NVS' THEN ' ' ELSE jl_reference_4 END
+             AS jl_reference_4,
+          CASE WHEN jl_reference_5 = 'NVS' THEN ' ' ELSE jl_reference_5 END
+             AS jl_reference_5,
+          CASE WHEN jl_reference_6 = 'NVS' THEN ' ' ELSE jl_reference_6 END
+             AS jl_reference_6,
+          CASE WHEN jl_reference_7 = 'NVS' THEN ' ' ELSE jl_reference_7 END
+             AS jl_reference_7,
+          CASE WHEN jl_reference_8 = 'NVS' THEN ' ' ELSE jl_reference_8 END
+             AS jl_reference_8,
+          CASE WHEN jl_reference_9 = 'NVS' THEN ' ' ELSE jl_reference_9 END
+             AS jl_reference_9,
+          CASE WHEN jl_reference_10 = 'NVS' THEN ' ' ELSE jl_reference_10 END
+             AS jl_reference_10,
+          CAST (jl_tran_ccy AS VARCHAR2 (3)) AS jl_tran_ccy,
+          CAST (jl_tran_amount AS NUMBER (23, 3)) AS jl_tran_amount,
           jl_base_rate,
-          cast(jl_base_ccy as varchar2(3)) as jl_base_ccy,
-          cast(jl_base_amount as number(23,3)) as jl_base_amount,
+          CAST (jl_base_ccy AS VARCHAR2 (3)) AS jl_base_ccy,
+          CAST (jl_base_amount AS NUMBER (23, 3)) AS jl_base_amount,
           jl_local_rate,
           jl_local_ccy,
           jl_local_amount,
@@ -52,15 +142,49 @@ create or replace view rdr.rcv_glint_journal_line (jl_jrnl_hdr_id, jl_jrnl_line_
           jl_amended_by,
           jl_amended_on,
           jl_recon_status,
-          nvl(fgl.lk_lookup_value2,' ') as event_group,
-          case when jl_tran_amount < 0 then jl_tran_amount else null end as credit_amt,
-          case when jl_tran_amount >= 0 then jl_tran_amount else null end as debit_amt,
-          'U' as event_status
-     from slr.slr_jrnl_lines jl
-          left join fdr.fr_general_lookup fgl
-             on     jl.jl_attribute_4 = fgl.lk_match_key1
-                and fgl.lk_lkt_lookup_type_code = 'EVENT_HIERARCHY'
-          left join fdr.fr_gl_account gl
-             on jl.jl_account = gl.ga_account_code
-    where 1 = 1;
+          NVL (fgl.lk_lookup_value3, ' ') AS event_class,
+          CASE WHEN jl_tran_amount < 0 THEN jl_tran_amount ELSE NULL END
+             AS credit_amt,
+          CASE WHEN jl_tran_amount >= 0 THEN jl_tran_amount ELSE NULL END
+             AS debit_amt,
+          'U' AS event_status,
+          jt.ejt_madj_flag
+     FROM slr.slr_jrnl_lines jl
+          LEFT JOIN fdr.fr_general_lookup fgl
+             ON     jl.jl_attribute_4 = fgl.lk_match_key1
+                AND fgl.lk_lkt_lookup_type_code = 'EVENT_HIERARCHY'
+          LEFT JOIN fdr.fr_gl_account gl
+             ON jl.jl_account = gl.ga_account_code
+          LEFT JOIN slr.slr_jrnl_headers jh
+             ON jh.jh_jrnl_id = jl.jl_jrnl_hdr_id
+          LEFT JOIN slr.slr_ext_jrnl_types jt
+             ON jt.ejt_type = jh.jh_jrnl_type
+    WHERE     jl.jl_amended_on >= SYSDATE - 365
+          AND (   EXISTS
+                     (SELECT NULL
+                        FROM fdr.fr_general_lookup fgl2
+                       WHERE     fgl.lk_lookup_value3 = fgl2.lk_match_key1
+                             AND fgl2.lk_lkt_lookup_type_code =
+                                    'EVENT_CLASS_PERIOD'
+                             AND fgl2.lk_lookup_value1 = 'C'
+                             AND jl.jl_period_year =
+                                    CAST (
+                                       fgl2.lk_match_key2 AS NUMBER (4, 0))
+                             AND jl.jl_period_month =
+                                    CAST (
+                                       fgl2.lk_match_key3 AS NUMBER (2, 0)))
+               OR EXISTS
+                     (SELECT NULL
+                        FROM fdr.fr_general_lookup fgl3
+                       WHERE     fgl.lk_lookup_value3 = fgl3.lk_match_key1
+                             AND fgl3.lk_lkt_lookup_type_code =
+                                    'EVENT_CLASS_PERIOD'
+                             AND fgl3.lk_lookup_value5 = 'Y'
+                             AND jl.jl_period_year =
+                                    CAST (
+                                       fgl3.lk_match_key2 AS NUMBER (4, 0))
+                             AND jl.jl_period_month =
+                                    CAST (
+                                       fgl3.lk_match_key3 AS NUMBER (2, 0))));
+
  comment on table rdr.rcv_glint_journal_line  IS 'Configurable View on Journal Lines that should be considered for sending to the GL.';
