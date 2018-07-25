120be2f6e49ec5e8710a593390a46bd8e7a529ad
Nulty, Thomas <TNulty@agltd.com>
2018-07-25 14:02:54 +0000

    Updated rr_glint_journal_line.sql

--- aahSubLedger/src/main/db/indices/rdr/rr_glint_journal_line.sql
+++ aahSubLedger/src/main/db/indices/rdr/rr_glint_journal_line.sql
@@ -1,2 +1,2 @@
 CREATE INDEX xif1gl_interface_journal_line ON rdr.rr_glint_journal_line (rgjl_rgj_id   ASC,rgjl_rgj_rgbc_id   ASC);
-CREATE INDEX ag_glint_je_line01 ON rdr.rr_glint_journal_line (ps_filter);
+CREATE INDEX idx_ag_glint_je_line01 ON rdr.rr_glint_journal_line (ps_filter);
