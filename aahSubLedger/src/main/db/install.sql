d170a93395146e12e5010ce19ce29923c311714b
jberner <jberner@assuredguaranty.com>
2018-08-01 11:01:36 -0400

    re checks

--- aahSubLedger/src/main/db/install.sql
+++ aahSubLedger/src/main/db/install.sql
@@ -254,8 +254,8 @@ conn ~rdr_logon
 @@indices/rdr/rr_glint_journal_line.sql
 @@views/rdr/rcv_glint_journal_line.sql
 @@views/rdr/rcv_glint_journal.sql
+@@views/rdr/rcv_ag_glint_ps_journals.sql
 @@tables/rdr/rr_glint_to_slr_ag.sql
-@@views/rdr/rrv_ag_glint_ps_journals.sql
 @@packages/rdr/pg_glint.hdr
 @@packages/rdr/pg_glint.bdy
 @@grants/packages/rdr/pg_glint.sql
@@ -286,7 +286,6 @@ conn ~fdr_logon
 
 conn ~slr_logon
 
-@@tables/slr/slr_fx_reval_temp.sql
 @@data/slr/slr_process_config.sql
 @@data/slr/slr_process_config_detail.sql
 @@data/slr/slr_process_source.sql
