943ccfc24adeaabea8d5f8cb81e823bba8b7c24f
jberner <jberner@assuredguaranty.com>
2018-08-09 10:33:04 -0400

    user octopus pw fix

--- aahETL/src/main/db/users/aah_ps.sql
+++ aahETL/src/main/db/users/aah_ps.sql
@@ -1,2 +1,2 @@
-create user aah_ps identified by "@aahPSPassword@";
+create user aah_ps identified by ~1;
 grant aah_glint to aah_ps;
--- aahETL/src/main/db/users/aah_read.sql
+++ aahETL/src/main/db/users/aah_read.sql
@@ -1,2 +1,2 @@
-create user aah_read identified by "@aahReadPassword@";
+create user aah_read identified by ~1;
 grant aah_read_only to aah_read;
