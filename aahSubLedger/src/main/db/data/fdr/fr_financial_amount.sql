ed2109a2b72d8f36c117a21bb5ad793ff48ee666
Kieft, Eric <EKieft@agltd.com>
2018-08-22 11:58:02 -0400

    zero transaction amt sign fix

--- aahSubLedger/src/main/db/data/fdr/fr_financial_amount.sql
+++ aahSubLedger/src/main/db/data/fdr/fr_financial_amount.sql
@@ -1,2 +1,3 @@
 insert into fdr.fr_financial_amount ( fa_table_name , fa_row_name ) values ( 'FR_STAN_RAW_ACC_EVENT' , 'SRAE_CLIENT_AMOUNT1' );
+insert into fdr.fr_financial_amount ( fa_table_name , fa_row_name ) values ( 'FR_STAN_RAW_ACC_EVENT' , 'SRAE_CLIENT_AMOUNT2' );
 commit;
