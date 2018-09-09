712e5bf905fbb1c0abd2914be93a645bdb6148b9
Kieft, Eric <EKieft@agltd.com>
2018-09-09 10:20:16 -0500

    new jl source type

--- aahStandardisation/src/main/db/data/stn/journal_line_source_type.sql
+++ aahStandardisation/src/main/db/data/stn/journal_line_source_type.sql
@@ -2,4 +2,5 @@ insert into stn.journal_line_source_type (SOURCE_TYP_CD, SOURCE_TYP_DESCR) value
 insert into stn.journal_line_source_type (SOURCE_TYP_CD, SOURCE_TYP_DESCR) values ('TC', 'Trade Credit Business');
 insert into stn.journal_line_source_type (SOURCE_TYP_CD, SOURCE_TYP_DESCR) values ('CA', 'Cash');
 insert into stn.journal_line_source_type (SOURCE_TYP_CD, SOURCE_TYP_DESCR) values ('PC', 'Profit Commissions');
+insert into stn.journal_line_source_type (SOURCE_TYP_CD, SOURCE_TYP_DESCR) values ('CR', 'Contingency Reserves');
 commit;
