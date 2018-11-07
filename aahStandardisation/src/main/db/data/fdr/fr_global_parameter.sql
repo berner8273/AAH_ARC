50311ec9076662ffadf53d892727a0720d151024
Berner, John <JBerner@agltd.com>
2018-11-07 10:13:22 -0500

    max_retiries update

--- aahStandardisation/src/main/db/data/fdr/fr_global_parameter.sql
+++ aahStandardisation/src/main/db/data/fdr/fr_global_parameter.sql
@@ -2,5 +2,6 @@ insert into fdr.fr_global_parameter ( gp_one_id , gp_tomorrows_bus_date , gp_tod
   values ( 2 , ( select gp_tomorrows_bus_date from fdr.fr_global_parameter where lpg_id = 1 ) , ( select gp_tomorrows_bus_date from fdr.fr_global_parameter where lpg_id = 1 ) - 1 , 33 , sysdate , 'Client Static' , 2 );
 
 update fr_global_parameter set gp_ca_processing_cal_name = 'AAH';
+update fr_global_parameter set gp_max_retries            = 99;
 
 commit;
