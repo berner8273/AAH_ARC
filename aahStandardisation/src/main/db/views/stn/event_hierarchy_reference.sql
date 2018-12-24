42fb8067705250470316f345b39b1fc28d1fec0e
Berner, John <JBerner@agltd.com>
2018-12-24 14:46:50 -0500

    new col in view

--- aahStandardisation/src/main/db/views/stn/event_hierarchy_reference.sql
+++ aahStandardisation/src/main/db/views/stn/event_hierarchy_reference.sql
@@ -11,8 +11,9 @@ select
      , fgl.lk_effective_from        effective_from
      , fgl.lk_effective_to          effective_to
      , fgl.lk_active                event_typ_sts
-  from
-       fdr.fr_general_lookup fgl
- where
-       fgl.lk_lkt_lookup_type_code = 'EVENT_HIERARCHY'
+     , gl.lk_lookup_value2          period_close_freq
+     from fdr.fr_general_lookup fgl
+        join fdr.fr_general_lookup gl on gl.lk_match_key1 = fgl.lk_lookup_value3
+            where gl.lk_lkt_lookup_type_code = 'EVENT_CLASS'        
+            and fgl.lk_lkt_lookup_type_code = 'EVENT_HIERARCHY'
 ;
