7002a16aa298371b2947f62fba59a60d8d1b8f87
Hine, Janet <JHine@agltd.com>
2019-02-14 14:01:13 +0000

    Updated cession_event.sql

--- aahStandardisation/src/main/db/indices/stn/cession_event.sql
+++ aahStandardisation/src/main/db/indices/stn/cession_event.sql
@@ -1,2 +1,2 @@
 create index i_cev_uuid on stn.cession_event ( correlation_uuid );
-create index idx_cession_event_acct_mon on stn.cession_event(stream_id,trunc("accounting_dt", 'fmmonth'));
+create index idx_cession_event_acct_mon on stn.cession_event(stream_id,trunc(accounting_dt, 'fmmonth'));
