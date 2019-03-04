ed1602bea71268d83866578127292b46a70a7cf0
Berner, John <JBerner@agltd.com>
2019-03-04 14:58:53 +0000

    Updated pg_combination_check.sql

--- aahSubLedger/src/main/db/grants/packages/fdr/pg_combination_check.sql
+++ aahSubLedger/src/main/db/grants/packages/fdr/pg_combination_check.sql
@@ -1,3 +1,4 @@
 grant execute on fdr.pg_combination_check to rdr;
 grant execute on fdr.pg_combination_check to slr;
 grant execute on fdr.pg_combination_check to gui;
+grant execute on fdr.pg_combination_check to stn;
