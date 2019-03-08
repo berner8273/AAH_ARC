33004aa7462182528df3afb9ebbf7802182484aa
Gaudet, Stephen <SGaudet@agltd.com>
2019-03-08 17:11:00 -0500

    update the custom install and uninstall to include input param

--- Custom_DB_install.sql
+++ Custom_DB_install.sql
@@ -1,17 +1,21 @@
 whenever sqlerror exit failure
 
+set define on
 set serveroutput on
 set echo on
 
+--Define Individual instance of User's Bubble
+    define b_num = &user_bubble_number
+
 
 --Define all other Connections
-	define 1 = 'FDR/bJGeTSP2PbMikw4d@Oraaptci-bubble1:1521:aptci'
-	define 2 = 'GUI/KWDfdtQqoa3A2nE5@Oraaptci-bubble1:1521:aptci'
-	define 3 = 'RDR/HuJJnYnTr8LRwJqL@Oraaptci-bubble1:1521:aptci'
+	define 1 = 'FDR/bJGeTSP2PbMikw4d@Oraaptci-bubble&b_num:1521:aptci'
+	define 2 = 'GUI/KWDfdtQqoa3A2nE5@Oraaptci-bubble&b_num:1521:aptci'
+	define 3 = 'RDR/HuJJnYnTr8LRwJqL@Oraaptci-bubble&b_num:1521:aptci'
 	define 4 = ''
-	define 5 = 'SLR/xJ4V8iyBJ7UuVSx6@Oraaptci-bubble1:1521:aptci'
-	define 6 = 'STN/NZ784FBLUJesbJvA@Oraaptci-bubble1:1521:aptci'
-	define 7 = 'SYS/ZMaz8DjieATJoxgM@Oraaptci-bubble1:1521:aptci'
+	define 5 = 'SLR/xJ4V8iyBJ7UuVSx6@Oraaptci-bubble&b_num:1521:aptci'
+	define 6 = 'STN/NZ784FBLUJesbJvA@Oraaptci-bubble&b_num:1521:aptci'
+	define 7 = 'SYS/ZMaz8DjieATJoxgM@Oraaptci-bubble&b_num:1521:aptci'
 	define 8 = ''
 
 --Install Scripts
@@ -30,20 +34,25 @@ set echo on
 		
 	--Running the Custom Step Trigger Install script
 		@@aahRDR/src/main/db/install.sql
-		
-		
+
+set define off		
+
+set define on
+
+--Define Individual instance of User's Bubble
+    define eb_num = &user_bubble_number
+	
 --Define ETL Connections
-	define 1 = 'SYS/ZMaz8DjieATJoxgM@Oraaptci-bubble1:1521:aptci'
+	define 1 = 'SYS/ZMaz8DjieATJoxgM@Oraaptci-bubble&eb_num:1521:aptci'
 	define 2 = 'rzj4rUD5XuG3cMNC'
 	define 3 = 'aahReadPassword'
 	define 4 = 'oAPnsamjBDWvE6de'
 	define 5 = 'hCkT85v3mckVbTwT'
-	define 6 = 'STN/NZ784FBLUJesbJvA@Oraaptci-bubble1:1521:aptci'
-	define 7 = 'SYS/ZMaz8DjieATJoxgM@Oraaptci-bubble1:1521:aptci'
+	define 6 = 'STN/NZ784FBLUJesbJvA@Oraaptci-bubble&eb_num:1521:aptci'
 	define 8 = ''
 
 	--Running the Custom Step Trigger Install script	
 		@@aahETL/src/main/db/install.sql	
 
-
+set define off
 exit
