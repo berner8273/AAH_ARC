37120e58fb3c78ac02434437ba24aa8bcd2c6b20
Gaudet, Stephen <SGaudet@agltd.com>
2019-03-08 16:54:26 -0500

    fixed issue with the rename statement

--- aahSubLedger/src/main/db/uninstall.sql
+++ aahSubLedger/src/main/db/uninstall.sql
@@ -147,7 +147,7 @@ drop view slr.v_slr_journal_lines;
 
 --Restore baseline view and drop modified view
 drop view slr.v_slr_jrnl_lines_unposted_jt;
-rename slr.v_slr_jrnl_lines_unposted_bak to slr.v_slr_jrnl_lines_unposted_jt;
+rename v_slr_jrnl_lines_unposted_bak to v_slr_jrnl_lines_unposted_jt;
 
 BEGIN
   FOR cur_rec IN (SELECT owner,
