e0adf31f36f00e6b2af91eb7372c99cd6e028446
Janet <jhine@agltd.com>
2019-03-13 14:49:14 -0400

    Change to get reins_co

--- aahGui/src/main/db/views/gui/vw_ui_reference_4.sql
+++ aahGui/src/main/db/views/gui/vw_ui_reference_4.sql
@@ -8,13 +8,20 @@ create or replace view gui.vw_ui_reference_4
    entity
 )
 as
-   select pl.pl_party_legal_clicode as "FDR_CODE",
-          pl.pl_full_legal_name as "FDR_DESCRIPTION",
-          pl.pl_party_legal_clicode as "LOOKUP_KEY",
-          pl.pl_si_sys_inst_id as "SOURCE_SYSTEM_ID",
-          null as "PARENT_ID",
-          null as "ENTITY"
-     from fdr.fr_party_legal pl
-    where pl.pl_pt_party_type_id in 
-    (select pt_party_type_id from fdr.fr_party_type where pt_party_type_name = 'Ledger Entity')        
+    select 
+        le.le_cd as "FDR_CODE",
+        le.le_descr as "FDR_DESCRIPTION",
+        le.le_cd as "LOOKUP_KEY",
+        'Client Static' as "SOURCE_SYSTEM_ID",
+        null as "PARENT_ID",
+        null as "ENTITY"    
+    from 
+        stn.legal_entity le
+    join 
+        stn.legal_entity_link lel on le_id=lel.child_le_id
+    where 
+        le.legal_entity_typ='INTERNAL'
+        and is_ledger_entity='N'
+        and lel.legal_entity_link_typ='SLR_LINK'
+    order by le.le_cd
 ;
