a7225659f9feb866966e35f90bfae3296f918da3
Berner, John <JBerner@agltd.com>
2019-03-20 16:16:31 -0400

    reverted commit

--- Custom_DB_uninstall.sql
+++ Custom_DB_uninstall.sql
@@ -1,16 +1,20 @@
-whenever sqlerror exit failure
+--whenever sqlerror exit failure
 
+set define on
 set serveroutput on
 set echo on
 
+--Define Individual instance of User's Bubble
+    define b_num = &user_bubble_number
+	
 --Define Connections
-	define 1 = 'FDR/bJGeTSP2PbMikw4d@Oraaptci-bubble1:1521:aptci'
-	define 2 = 'GUI/KWDfdtQqoa3A2nE5@Oraaptci-bubble1:1521:aptci'
-	define 3 = 'RDR/HuJJnYnTr8LRwJqL@Oraaptci-bubble1:1521:aptci'
+	define 1 = 'FDR/bJGeTSP2PbMikw4d@Oraaptci-bubble&b_num:1521:aptci'
+	define 2 = 'GUI/KWDfdtQqoa3A2nE5@Oraaptci-bubble&b_num:1521:aptci'
+	define 3 = 'RDR/HuJJnYnTr8LRwJqL@Oraaptci-bubble&b_num:1521:aptci'
 	define 4 = ''
-	define 5 = 'SLR/xJ4V8iyBJ7UuVSx6@Oraaptci-bubble1:1521:aptci'
-	define 6 = 'STN/NZ784FBLUJesbJvA@Oraaptci-bubble1:1521:aptci'
-	define 7 = 'SYS/ZMaz8DjieATJoxgM@Oraaptci-bubble1:1521:aptci'
+	define 5 = 'SLR/xJ4V8iyBJ7UuVSx6@Oraaptci-bubble&b_num:1521:aptci'
+	define 6 = 'STN/NZ784FBLUJesbJvA@Oraaptci-bubble&b_num:1521:aptci'
+	define 7 = 'SYS/ZMaz8DjieATJoxgM@Oraaptci-bubble&b_num:1521:aptci'
 	define 8 = ''
 
 
@@ -34,5 +38,5 @@ set echo on
 	--Running the Custom Step Trigger Install script	
 		@@aahETL/src/main/db/uninstall.sql
 		
-
+set define off
 exit
