8d8996f690874fd6843646a8cd6a697d2b1d1d3d
Gaudet, Stephen <SGaudet@agltd.com>
2019-03-28 12:39:10 -0400

    updated rdr_pkg and rr_glint_to_slr_ag

--- aahSubLedger/src/main/db/indices/rdr/rr_glint_to_slr_ag.sql
+++ aahSubLedger/src/main/db/indices/rdr/rr_glint_to_slr_ag.sql
@@ -1,2 +1,2 @@
-CREATE INDEX rdr.idx_rr_glint_to_slr_ag_hdr ON rdr.rr_glint_to_slr_ag (jl_jrnl_hdr_id);
-ALTER TABLE rdr.rr_glint_to_slr_ag ADD CONSTRAINT "xpk_rr_glint_to_slr_ag" PRIMARY KEY ("RGJL_ID");
+ALTER TABLE rdr.rr_glint_to_slr_ag ADD CONSTRAINT xpk_rr_glint_to_slr_ag PRIMARY KEY (RGJL_ID, JL_JRNL_HDR_ID, JL_JRNL_LINE_NUMBER);
+CREATE UNIQUE INDEX rdr.idx_rr_glint_to_slr_ag_hdr ON rdr.rr_glint_to_slr_ag (JL_JRNL_HDR_ID, JL_JRNL_LINE_NUMBER);
