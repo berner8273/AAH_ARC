d7c8bf06736737c91eb87d801dd934789f29c611
Berner, John <JBerner@agltd.com>
2019-04-09 15:20:25 +0000

    Updated install.sql

--- aahSubLedger/src/main/db/install.sql
+++ aahSubLedger/src/main/db/install.sql
@@ -412,8 +412,8 @@ alter package slr.slr_utilities_pkg compile;
 alter package slr.slr_validate_journals_pkg compile;
 
 conn ~gui_logon
-@@packages/gui/pgui_manual_journal.bdy
 @@packages/gui/pgui_manual_journal.hdr
+@@packages/gui/pgui_manual_journal.bdy
 
 conn ~fdr_logon
 alter package fdr.pk_legal_entity compile;
