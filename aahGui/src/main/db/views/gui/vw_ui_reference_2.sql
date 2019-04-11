83de389fbf8a381b358dc792b23d7a7c6834d3bd
Janet <jhine@agltd.com>
2019-04-11 14:27:25 -0400

    remove stn table use

--- aahGui/src/main/db/views/gui/vw_ui_reference_2.sql
+++ aahGui/src/main/db/views/gui/vw_ui_reference_2.sql
@@ -8,13 +8,21 @@ create or replace view gui.vw_ui_reference_2
    entity
 )
 as
-   select pl.pl_party_legal_clicode as "FDR_CODE",
-          pl.pl_full_legal_name as "FDR_DESCRIPTION",
-          pl.pl_party_legal_clicode as "LOOKUP_KEY",
-          pl.pl_si_sys_inst_id as "SOURCE_SYSTEM_ID",
-          null as "PARENT_ID",
-          null as "ENTITY"
-     from fdr.fr_party_legal pl
-    where pl.pl_pt_party_type_id in 
-    (select pt_party_type_id from fdr.fr_party_type where pt_party_type_name = 'Ledger Entity')        
-;
+        select
+        pl.pl_party_legal_clicode as "FDR_CODE",
+        pl.pl_full_legal_name as "FDR_DESCRIPTION",
+        pl.pl_party_legal_clicode as "LOOKUP_KEY",
+        pl.pl_si_sys_inst_id as "SOURCE_SYSTEM_ID",
+        null as "PARENT_ID",
+        null as "ENTITY"
+    from fdr.fr_party_legal pl
+        join fdr.fr_party_type fpt 
+            on pl.pl_pt_party_type_id = fpt.pt_party_type_id
+        join fdr.fr_org_network fon
+            on pl.pl_party_legal_id = fon.on_org_node_client_code
+        join fdr.fr_org_node_structure fons
+            on fons.ons_on_child_org_node_id = fon.on_org_node_id
+    where pl.pl_active = 'A'
+      and pl.pl_pt_party_type_id ='10'
+      and fons.ons_oht_org_hier_type_id = '6'
+    order by pl.pl_party_legal_clicode;
