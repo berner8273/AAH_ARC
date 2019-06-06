f3c55ddd8219f6f32f750fd33c39e579c6b2bd59
Quigley, Ryan <RQuigley@assuredguaranty.com>
2019-06-05 22:33:43 -0400

    remove hint from full build

--- aahSubLedger/src/main/db/install.sql
+++ aahSubLedger/src/main/db/install.sql
@@ -427,10 +427,5 @@ conn ~stn_logon
 alter package stn.pk_jl compile;
 alter package stn.pk_cev compile;
 
-conn ~slr_logon
-update slr.SLR_HINTS_SETS 
-set hs_hint = null
-where HS_STATEMENT = 'IMPORT_INSERT_UNPOSTED';
-commit;
 
 
