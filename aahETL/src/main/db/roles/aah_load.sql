8b08efe849b58a0b99fbc59a50e77cdbd937abb8
Quigley, Ryan <RQuigley@assuredguaranty.com>
2019-06-18 16:41:12 -0400

    fix upgrade

--- aahETL/src/main/db/roles/aah_load.sql
+++ aahETL/src/main/db/roles/aah_load.sql
@@ -37,5 +37,6 @@ grant 		   select 		   on SLR.SLR_JRNL_LINES_UNPOSTED to aah_load;
 grant 		   select 		   on fdr.fr_log to aah_load;
 grant 		   select 		   on fdr.fr_general_codes to aah_load;
 grant 		   select 		   on fdr.fr_general_lookup to aah_load;
+grant 		   execute 		   on stn.f_event_status_desc to aah_load;
 
 
