77576d42799c2976d9ab6f415ec70b67b2a0589b
Quigley, Ryan <RQuigley@assuredguaranty.com>
2019-07-18 16:27:04 -0400

    add update to aah_load

--- aahETL/src/main/db/roles/aah_load.sql
+++ aahETL/src/main/db/roles/aah_load.sql
@@ -21,7 +21,7 @@ grant insert , select          on stn.insurance_policy_fx_rate    to aah_load;
 grant insert , select          on stn.cession                     to aah_load;
 grant insert , select          on stn.cession_link                to aah_load;
 grant insert , select, delete  on stn.cession_event               to aah_load;
-grant insert , select          on stn.journal_line                to aah_load;
+grant insert , select, update  on stn.journal_line                to aah_load;
 grant insert , select          on stn.tax_jurisdiction            to aah_load;
 grant          select          on stn.event_type                  to aah_load;
 grant insert , select          on stn.event_hierarchy             to aah_load;
