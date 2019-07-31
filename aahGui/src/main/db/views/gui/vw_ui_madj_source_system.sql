25d292212fd449933a4e3f402d9b9275ba1e9302
Berner, John <JBerner@agltd.com>
2019-07-31 11:29:28 -0400

    Merged PR 908: Merge 1.9.1-B42110 to 1.9.1
    Added new jrnl_source MADJGEN-OTHER

--- aahGui/src/main/db/views/gui/vw_ui_madj_source_system.sql
+++ aahGui/src/main/db/views/gui/vw_ui_madj_source_system.sql
@@ -11,6 +11,8 @@ AS
      FROM (SELECT 'MADJGEN' si_sys_inst_id, 'MADJ General' si_sys_inst_name FROM DUAL
              UNION ALL
            SELECT 'MADJGEN-CR' si_sys_inst_id, 'Contingency Reserves' si_sys_inst_name FROM DUAL
+           UNION ALL  
+           SELECT 'MADJGEN-OTHER' si_sys_inst_id, 'Other' si_sys_inst_name FROM DUAL             
             UNION ALL
            SELECT 'MADJGEN-USGAAPFX' si_sys_inst_id, 'US GAAP FX' si_sys_inst_name FROM DUAL
             UNION ALL
