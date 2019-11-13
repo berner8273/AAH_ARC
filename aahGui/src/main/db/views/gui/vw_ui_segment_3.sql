160d3a66bcba718230635b655fa4d70ecdbd59f5
Berner, John <JBerner@agltd.com>
2019-11-13 16:28:08 -0500

    Merged PR 940: manual_journal department issues
    manual_journal department issues
    
    Related work items: #42725

--- aahGui/src/main/db/views/gui/vw_ui_segment_3.sql
+++ aahGui/src/main/db/views/gui/vw_ui_segment_3.sql
@@ -9,7 +9,7 @@ create or replace view gui.vw_ui_segment_3
 )
 as
    select frb.bo_book_clicode as "FDR_CODE",
-          frb.bo_book_name as "FDR_DESCRIPTION",
+          frb.bo_book_clicode||' - '|| frb.bo_book_name as "FDR_DESCRIPTION",
           frb.bo_book_clicode as "LOOKUP_KEY",
           'Client Static' as "SOURCE_SYSTEM_ID",
           null as "PARENT_ID",
