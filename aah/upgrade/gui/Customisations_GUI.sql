e160b2d715ddf1445523a9e2a3a19753450fee4c
Berner, John <JBerner@agltd.com>
2020-12-14 19:10:30 +0000

    Updated Customisations_GUI.sql

--- aah/upgrade/gui/Customisations_GUI.sql
+++ aah/upgrade/gui/Customisations_GUI.sql
@@ -2,7 +2,7 @@ spool Customisations_GUI.log
 set echo on
 
 -- Load the GUI package
-@@PGUI_MANUAL_JOURNAL.sql
-aah/aahCustom/aahSubLedger/src/main/db/packages/gui
+@@../aahCustom/aahSubLedger/src/main/db/packages/gui/pgui_manual_journal.hdr
+@@../aahCustom/aahSubLedger/src/main/db/packages/gui/pgui_manual_journal.bdy
 
 show errors
