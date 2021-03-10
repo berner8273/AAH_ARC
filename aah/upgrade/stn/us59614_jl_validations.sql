7c81805fcf611a6c6e8f0248f7fee49f2808a97e
Berner, John <JBerner@agltd.com>
2021-03-10 15:01:14 -0500

    add install upgrade script

---
+++ aah/upgrade/stn/us59614_jl_validations.sql
@@ -0,0 +1,4 @@
+@@../aahCustom/aahStandardisation/src/main/db/packages/stn/pk_jl.bdy
+@@../aahCustom/aahStandardisation/src/main/db/data/stn/validation.sql
+@@../aahCustom/aahStandardisation/src/main/db/data/stn/validation_column.sql
+@@../aahCustom/aahStandardisation/src/main/db/data/stn/validation_type.sql
