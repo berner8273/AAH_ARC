2c955a52eaadd3067ec95afdeb040809947b5d9c
Vaynshteyn, Margarita <MVaynshteyn@agltd.com>
2021-03-18 18:53:47 -0400

    User Story 58323: Special Rule when importing in PS FV UPR Events booked in UK and FR Gaap Ledgers
    Related Work Items: #58323, #59112

--- aah/upgrade/install.sql
+++ aah/upgrade/install.sql
@@ -30,29 +30,18 @@ conn ~sys_logon
 /* Begin AAH custom upgrades */
 
 conn ~slr_logon
-@@slr/Customisations_SLR.sql;
+
 
 conn ~stn_logon
-@@stn/us44041_period_status.sql;
-@@stn/hotfix_cev_hint2.sql;
-@@stn/BUG58333_missing_vie_events.sql;
 
 conn ~gui_logon
-@@gui/Customisations_GUI.sql;
-@@gui/us44041_ui_field.sql;
 
 conn ~rdr_logon
---@@rdr/Customisations_RDR.sql --moved this to a separate branch and deploy
-@@rdr/us53039_rcv_glint_journal_line_views.sql;
-@@rdr/us44041_rdr_pkg_bdy.sql;
-@@rdr/pg_glint_hdr.sql;
+@@rdr/us58323_rcv_glint_journal_line_views.sql
 
 conn ~fdr_logon
-@@fdr/purge_dup_combo_rules.sql;
-@@fdr/add_gen_lookup_close_trigger.sql;
 
 conn ~sys_logon
-@@sys/add_back_security.sql;
 
 /* End AAH custom upgrades */
 
---
+++ aah/upgrade/rdr/us58323_rcv_glint_journal_line_views.sql
@@ -0,0 +1,4 @@
+@@../aahCustom/aahSubLedger/src/main/db/views/rdr/RCV_GLINT_JOURNAL_LINE.sql
+@@../aahCustom/aahRDR/src/main/db/views/rdr/RRV_AG_GLINT_JL_MANUAL.sql
+@@../aahCustom/aahRDR/src/main/db/views/rdr/RRV_AG_GLINT_JL_UNPOSTED.sql
+@@../aahCustom/aahSubLedger/src/main/db/views/rdr/RRV_AG_GLINT_JRNL_LINES.sql
