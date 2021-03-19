393cde6a4cccdc702d7dc8e3da905b22381af2f3
Vaynshteyn, Margarita <MVaynshteyn@agltd.com>
2021-03-19 14:52:40 -0400

    User Story 58323: Special Rule . Path to SQL is fixed to CASE sensitive.
    Related Work Items: #58323, #59888

--- aah/upgrade/rdr/us58323_rcv_glint_journal_line_views.sql
+++ aah/upgrade/rdr/us58323_rcv_glint_journal_line_views.sql
@@ -1,4 +1,4 @@
-@@../aahcustom/aahsubledger/src/main/db/views/rdr/rcv_glint_journal_line.sql
-@@../aahcustom/aahrdr/src/main/db/views/rdr/rrv_ag_glint_jl_manual.sql
-@@../aahcustom/aahrdr/src/main/db/views/rdr/rrv_ag_glint_jl_unposted.sql
-@@../aahcustom/aahsubledger/src/main/db/views/rdr/rrv_ag_glint_jrnl_lines.sql
+@@../aahCustom/aahSubLedger/src/main/db/views/rdr/rcv_glint_journal_line.sql
+@@../aahCustom/aahRDR/src/main/db/views/rdr/rrv_ag_glint_jl_manual.sql
+@@../aahCustom/aahRDR/src/main/db/views/rdr/rrv_ag_glint_jl_unposted.sql
+@@../aahCustom/aahSubLedger/src/main/db/views/rdr/rrv_ag_glint_jrnl_lines.sql
