392cc6875eff4aa0ce72b8f88cfd2f8ced85bec1
Vaynshteyn, Margarita <MVaynshteyn@agltd.com>
2021-03-18 21:26:01 -0400

    User Story 58323: Special Rule when importing in PS FV UPR Events booked in UK and FR Gaap Ledgers

--- aah/upgrade/rdr/us58323_rcv_glint_journal_line_views.sql
+++ aah/upgrade/rdr/us58323_rcv_glint_journal_line_views.sql
@@ -1,2 +1,4 @@
 @@../aahcustom/aahsubledger/src/main/db/views/rdr/rcv_glint_journal_line.sql
-
+@@../aahcustom/aahrdr/src/main/db/views/rdr/rrv_ag_glint_jl_manual.sql
+@@../aahcustom/aahrdr/src/main/db/views/rdr/rrv_ag_glint_jl_unposted.sql
+@@../aahcustom/aahsubledger/src/main/db/views/rdr/rrv_ag_glint_jrnl_lines.sql
