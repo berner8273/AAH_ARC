269773759ed7adc19824ef5c4a626ea3f05af054
Berner, John <JBerner@agltd.com>
2021-04-12 10:30:07 -0400

    added archive grants

---
+++ aah/upgrade/sys/archive_grants.sql
@@ -0,0 +1,9 @@
+
+grant create session to ARC;
+Grant "CONNECT" to "ARC";
+Grant Create Table to "ARC";
+GRANT CREATE PROCEDURE TO "ARC" ;
+Grant Execute on SYS.DBMS_ROWID to "ARC";
+grant SELECT on "STN"."FEED" to "FDR" ;
+grant DELETE, SELECT on "STN"."LEGAL_ENTITY" to "FDR" ;
+grant DELETE, SELECT on "STN"."LEGAL_ENTITY_LINK" to "FDR" ;
