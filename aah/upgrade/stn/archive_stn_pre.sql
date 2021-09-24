2e97457193e233e54e43ce8c0bb1f5f20ea0fd3e
Berner, John <JBerner@agltd.com>
2021-09-24 17:52:54 +0000

    Updated archive_stn_pre.sql

--- aah/upgrade/stn/archive_stn_pre.sql
+++ aah/upgrade/stn/archive_stn_pre.sql
@@ -23,13 +23,12 @@ select count(*) into nSavedCount from (
 select t.* FROM STN.CESSION_EVENT t JOIN STN.FEED f ON t.feed_uuid = f.feed_uuid WHERE t.event_status in ('P','E','X') and f.loaded_ts > SYSDATE  - 320);
 
 execute immediate 'insert into CESSION_EVENT_BAK '||q'[(SELECT t.* FROM STN.CESSION_EVENT t JOIN STN.FEED f ON t.feed_uuid = f.feed_uuid WHERE t.event_status in ('P','E','X') and f.loaded_ts > SYSDATE  - 320)]';
-dbms_output.put_line('inserted');          
 
 select count(*) into nCount from cession_event_bak;
 IF nCount = nSavedCount THEN
  execute immediate 'truncate table cession_event';
  execute immediate 'insert /*+APPEND*/ into cession_event (select * from cession_event_bak)';
-dbms_output.put_line('insert cev '||to_char(nCount)||' : '||to_char(nSavedCount));
+
 END IF;  
 
 -- setup for cession delete
