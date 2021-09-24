8e958eb28836b791e45d0d74c3a71d8ec8687ff8
Berner, John <JBerner@agltd.com>
2021-09-24 20:29:54 +0000

    Updated archive_stn_pre.sql

--- aah/upgrade/stn/archive_stn_pre.sql
+++ aah/upgrade/stn/archive_stn_pre.sql
@@ -28,11 +28,51 @@ select count(*) into nCount from cession_event_bak;
 IF nCount = nSavedCount THEN
  execute immediate 'truncate table cession_event';
  execute immediate 'insert /*+APPEND*/ into cession_event (select * from cession_event_bak)';
+END IF;  
+
+end;
+
+declare
+nCount1  NUMBER;
+
+begin
+
+ execute immediate 'alter table stn.cession_link disable constraint fk_cc_cl';
+ execute immediate 'alter table stn.cession_link disable constraint fk_pc_cl';
+
+SELECT count(*) into nCount1 FROM dba_tables where upper(table_name) = 'CESSION_BAK';
+
+IF nCount1 = 0  THEN  
+   execute immediate 'create table cession_bak as select * from cession where 1 = 2';
+ELSE
+    execute immediate 'truncate table cession_bak';
+END IF;
+
+END;
+
+declare
+nCount  NUMBER;
+nSavedCount NUMBER;
+
+begin
+
+select count(*) into nSavedCount from (
+select t.* FROM STN.cession t JOIN STN.FEED f ON t.feed_uuid = f.feed_uuid WHERE t.event_status in ('P','E','X') and f.loaded_ts > SYSDATE  - 320);
+
+execute immediate 'insert into cession_BAK '||q'[(SELECT t.* FROM STN.cession t JOIN STN.FEED f ON t.feed_uuid = f.feed_uuid WHERE t.event_status in ('P','E','X') and f.loaded_ts > SYSDATE  - 320)]';          
+
+select count(*) into nCount from cession_bak;
+IF nCount = nSavedCount THEN
+ 
+ execute immediate 'truncate table cession';
+ execute immediate 'insert /*+APPEND*/ into cession (select * from cession_bak)';
 
 END IF;  
 
--- setup for cession delete
---    execute immediate 'alter table stn.cession_link disable constraint fk_cc_cl';
---    execute immediate 'alter table stn.cession_link disable constraint fk_pc_cl';
+commit;
+
+execute immediate 'drop table cession_bak';
+execute immediate 'drop table cession_event_bak';
 
 end;
+/
