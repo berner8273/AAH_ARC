3780d5392b99525eb6f65be25bbd31bea2623c14
Berner, John <JBerner@agltd.com>
2021-09-27 21:06:50 +0000

    Updated archive_stn_setup.sql

--- aah/upgrade/stn/archive_stn_setup.sql
+++ aah/upgrade/stn/archive_stn_setup.sql
@@ -5,14 +5,10 @@ begin
 
 SELECT count(*) into nCount1 FROM dba_tables where upper(table_name) = 'CESSION_EVENT_BAK';
 
-DBMS_OUTPUT.PUT_LINE(' count1 '||to_char(nCount1));
-
 IF nCount1 = 0  THEN  
    execute immediate 'create table cession_event_bak as select * from cession_event where 1 = 2';
-   DBMS_OUTPUT.PUT_LINE(' create' );
 ELSE
     execute immediate 'truncate table cession_event_bak';
-    DBMS_OUTPUT.PUT_LINE(' truncate ' );
 END IF;
 
 END;
@@ -29,16 +25,15 @@ begin
 select count(*) into nSavedCount from (
 select t.* FROM STN.CESSION_EVENT t JOIN STN.FEED f ON t.feed_uuid = f.feed_uuid WHERE t.event_status in ('P','E','X') and f.loaded_ts > SYSDATE  - (select arct_archive_days from fdr.fr_archive_ctl where arct_table_name = 'CESSION_EVENT'));
 
-DBMS_OUTPUT.PUT_LINE(' countSavedcount '||to_char(nSavedCount));
-
 execute immediate 'insert into CESSION_EVENT_BAK '||q'[(SELECT t.* FROM STN.CESSION_EVENT t JOIN STN.FEED f ON t.feed_uuid = f.feed_uuid WHERE t.event_status in ('P','E','X') and f.loaded_ts > SYSDATE  - (select arct_archive_days from fdr.fr_archive_ctl where arct_table_name = 'CESSION_EVENT'))]';
 
 select count(*) into nCount from cession_event_bak;
 IF nCount = nSavedCount THEN
-dbms_output.put_line(to_char(nCount)||' : '||to_char(nSavedCount));
  execute immediate 'truncate table cession_event';
  execute immediate 'insert /*+APPEND*/ into cession_event (select * from cession_event_bak)';
 END IF;  
+
+commit;
 
 end;
 /
