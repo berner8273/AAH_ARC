5304ebb1aaaea336cc20a642ebc043c9d0b2964d
Berner, John <JBerner@agltd.com>
2021-09-27 13:44:01 +0000

    Updated install.sql

--- aah/upgrade/install.sql
+++ aah/upgrade/install.sql
@@ -29,55 +29,8 @@ conn ~sys_logon
 /************************************** Begin AAH custom upgrades *****************************************************/
 
 conn ~stn_logon
-@@stn/archive_stn_insert.sql
+@@stn/archive_stn_setup.sql
 
-conn ~gui_logon
-
-conn ~fdr_logon
-@@fdr/FR_ARCHIVE_CTL_Insert.sql
-@@fdr/archive_fdr_insert.sql
-@@fdr/archive_fdr_custom_pkg.sql
-
-conn ~rdr_logon
-conn ~sys_logon as sysdba
-
-    DECLARE space_exists number;
-	BEGIN
-		select count(*) into space_exists from dba_tablespaces where tablespace_name = 'ARC_DATA';
-		IF (space_exists = 0) THEN
-			EXECUTE IMMEDIATE '
-			create bigfile tablespace ARC_DATA
-			datafile ''/oradata/~oraServiceName/datafile/ARC_DATA_FILE.dbf''    
-			size 10g autoextend on next 1g maxsize 20G';
-		END IF;
-	END;
-	/
-	DECLARE user_exists number;
-	BEGIN
-		select count(*) into user_exists from dba_users where username='ARC';
-		IF (user_exists = 0) THEN
-        EXECUTE IMMEDIATE '
-			create user ARC identified by "~arcPwd"
-				default tablespace ARC_DATA
-				temporary tablespace temp
-				quota unlimited on ARC_DATA
-				profile default
-				account unlock';
-		END IF;
-	END;
-    /
-	grant create session to ARC;
-	grant CONNECT to ARC;
-	ALTER USER ARC DEFAULT ROLE CONNECT;
-
-conn ~sys_logon as sysdba
-@@sys/archive_grants.sql
-
-conn ~arc_logon
-@@arc/arc_setup.sql
-
-conn ~sys_logon as sysdba
-@@sys/archive_grants_post.sql
 
 /********************************* End AAH custom upgrades ********************************************************/
 
