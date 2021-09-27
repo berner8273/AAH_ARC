b047f4a39eb21b45a4889201f4b3c9fc820f579b
Berner, John <JBerner@agltd.com>
2021-09-27 13:40:59 +0000

    Updated archive_testing_script.sql

--- aah/utilities/archive_testing_script.sql
+++ aah/utilities/archive_testing_script.sql
@@ -1,6 +1,3 @@
--- before running archive scripts make sure this is run as aptitude user:
--- grant alter any table to fdr
-
 DECLARE
   v_sql varchar2(1256);
   v_count_arc integer :=0;
@@ -8,13 +5,14 @@ DECLARE
   v_count2 integer :=0;
   v_count3 number :=0;
   v_count4 number :=0;
+  v_lpg_id number;
   bus_date  varchar2(10);
   bus_dateS varchar2(10);
   
   CURSOR c_fdr
   IS
     SELECT 
-        arct_table_name t_name,arct_arc_table_name a_name,arct_archive_days a_days,arct_archive_date_column a_date,arct_schema_name schema
+        arct_id,arct_table_name t_name,arct_arc_table_name a_name,arct_archive_days a_days,arct_archive_date_column a_date,arct_schema_name schema
     FROM 
         fdr.fr_archive_ctl 
     WHERE
@@ -23,7 +21,7 @@ DECLARE
   CURSOR c_stn
   IS
     SELECT 
-        arct_table_name t_name,arct_arc_table_name a_name,arct_archive_days a_days,arct_archive_date_column a_date,arct_archive_where_clause wc,arct_schema_name schema
+        arct_id,arct_table_name t_name,arct_arc_table_name a_name,arct_archive_days a_days,arct_archive_date_column a_date,arct_archive_where_clause wc,arct_schema_name schema
     FROM 
         fdr.fr_archive_ctl 
     WHERE
@@ -31,8 +29,8 @@ DECLARE
        
 BEGIN
 
-dbms_output.put_line(q'['A','B','C','D','E','F']');
-dbms_output.put_line(q'['Table','Schema','Rows Need Archive','Total Rows','Archived','Archive Log']');
+dbms_output.put_line(q'['1','2','A','B','C','D','E','F']');
+dbms_output.put_line(q'['ID','Group','Table','Schema','Rows Need Archive','Total Rows','Archived','Archive Log']');
 
   select to_char(gp_todays_bus_date,'mm/dd/yyyy') into bus_date from fdr.fr_global_parameter where lpg_id=1;
   select to_char(sysdate,'mm/dd/yyyy') into bus_dateS from dual;
@@ -48,6 +46,10 @@ dbms_output.put_line(q'['Table','Schema','Rows Need Archive','Total Rows','Archi
     
     v_sql:= 'select count(*) into :v_count_arc from all_tables where owner = ''ARC'' and table_name = '||''''||r_fdr.t_name||'''';
     EXECUTE IMMEDIATE v_sql INTO v_count_arc;                
+  
+    v_sql:= 'select lpg_id into :v_lpg_id from fdr.'||r_fdr.t_name||' where ROWNUM = 1';
+    EXECUTE IMMEDIATE v_sql INTO v_lpg_id;                
+  
     
    IF v_count_arc > 0 THEN
         v_sql := 'select count(*) INTO :v_count3 from ARC.'||r_fdr.a_name||q'[ where event_status in ('P','E') and ]'||r_fdr.a_date||q'[ <= to_date(']'||bus_date||q'[','mm/dd/yyyy')]'||'  - '||r_fdr.a_days;   
@@ -55,7 +57,7 @@ dbms_output.put_line(q'['Table','Schema','Rows Need Archive','Total Rows','Archi
     ELSE
         v_count3 :=0;
     END IF;                
-  
+        
     BEGIN 
     v_sql := 'select nvl(FL.ARL_RECORDS_ARCHIVED,0) INTO :v_count4 from fdr.fr_archive_ctl fc, fdr.fr_archive_log fl where FC.ARCT_ID = fl.arl_arct_id and fc.arct_table_name = '||''''||r_fdr.t_name||''''||' and fl.arl_arlh_id = (select max(arl_arlh_id) from fdr.fr_archive_log)';
     EXECUTE IMMEDIATE v_sql INTO v_count4;
@@ -64,7 +66,7 @@ dbms_output.put_line(q'['Table','Schema','Rows Need Archive','Total Rows','Archi
      v_count4:=0;
     END;    
            
-   dbms_output.put_line( r_fdr.t_name||','||r_fdr.schema||','||v_count1||','||v_count2||','||v_count3||','||v_count4);
+   dbms_output.put_line(r_fdr.arct_id||','||v_lpg_id||','|| r_fdr.t_name||','||r_fdr.schema||','||v_count1||','||v_count2||','||v_count3||','||v_count4);
     
   END LOOP;
   
@@ -76,7 +78,16 @@ dbms_output.put_line(q'['Table','Schema','Rows Need Archive','Total Rows','Archi
 
    v_sql := 'select count(*) from stn.'||r_stn.t_name;
    EXECUTE IMMEDIATE v_sql INTO v_count2; 
+
+
+   v_sql:= 'select count(*) into :v_lpg_id from stn.'||r_stn.t_name||' where ROWNUM = 1';
+   EXECUTE IMMEDIATE v_sql INTO v_lpg_id;
    
+   IF v_lpg_id > 0 THEN        
+    v_sql:= 'select lpg_id into :v_lpg_id from stn.'||r_stn.t_name||' where ROWNUM = 1';
+    EXECUTE IMMEDIATE v_sql INTO v_lpg_id;                
+   END IF;
+    
     BEGIN 
         v_sql := 'select FL.ARL_RECORDS_ARCHIVED INTO :v_count4 from fdr.fr_archive_ctl fc, fdr.fr_archive_log fl where FC.ARCT_ID = fl.arl_arct_id  and fc.arct_table_name = '||''''||r_stn.t_name||''''||' and fl.arl_arlh_id = (select max(arl_arlh_id) from fdr.fr_archive_log)';
         EXECUTE IMMEDIATE v_sql INTO v_count4;
@@ -87,7 +98,7 @@ dbms_output.put_line(q'['Table','Schema','Rows Need Archive','Total Rows','Archi
     
     v_count3:=0;
                 
-   dbms_output.put_line( r_stn.t_name||','||r_stn.schema||' '||','||v_count1||','||v_count2||','||v_count3||','||v_count4);    
+   dbms_output.put_line(r_stn.arct_id||','||v_lpg_id||','|| r_stn.t_name||','||r_stn.schema||' '||','||v_count1||','||v_count2||','||v_count3||','||v_count4);    
     
  END LOOP;    
 
