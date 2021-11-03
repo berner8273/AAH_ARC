9e3d773560bfdd68b4b151035fc28189b1a2c1d7
Berner, John <JBerner@agltd.com>
2021-11-03 19:39:29 +0000

    Updated us53060_stn_columns.sql

--- aah/upgrade/stn/us53060_stn_columns.sql
+++ aah/upgrade/stn/us53060_stn_columns.sql
@@ -24,6 +24,17 @@ BEGIN
       commit;
   end if;
 
+  Select count(*) into v_column_exists
+    from ALL_TAB_COLUMNS
+    where upper(column_name) = 'JL_DESCRIPTION'
+      and upper(table_name) = 'CESSION_EVENT'
+      and owner = 'STN' ;
+
+  if (v_column_exists = 0) then
+      execute immediate 'alter table stn.cession_event add (JL_DESCRIPTION VARCHAR2(80))';
+      commit;
+  end if;
+
   -- add column to posting_account_derivation
   Select count(*) into v_column_exists
     from ALL_TAB_COLUMNS
@@ -72,6 +83,19 @@ BEGIN
       commit;
   end if;
 
+  -- add column to cev_vie_data
+  Select count(*) into v_column_exists
+    from ALL_TAB_COLUMNS
+    where upper(column_name) = 'JL_DESCRIPTION'
+      and upper(table_name) = 'CEV_VIE_DATA'
+      and owner = 'STN' ;
+
+  if (v_column_exists = 0) then
+      execute immediate 'alter table stn.cev_vie_data add (JL_DESCRIPTION VARCHAR2(80))';
+      commit;
+  end if;
+
+
   -- add column to cev_intercompany_data
   Select count(*) into v_column_exists
     from ALL_TAB_COLUMNS
@@ -84,6 +108,18 @@ BEGIN
       commit;
   end if;
 
+  -- add column to cev_intercompany_data
+  Select count(*) into v_column_exists
+    from ALL_TAB_COLUMNS
+    where upper(column_name) = 'JL_DESCRIPTION'
+      and upper(table_name) = 'CEV_INTERCOMPANY_DATA'
+      and owner = 'STN' ;
+
+  if (v_column_exists = 0) then
+      execute immediate 'alter table stn.cev_intercompany_data add (JL_DESCRIPTION VARCHAR2(80))';
+      commit;
+  end if;
+
   -- add column to cev_non_intercompany_data
   Select count(*) into v_column_exists
     from ALL_TAB_COLUMNS
@@ -93,6 +129,18 @@ BEGIN
 
   if (v_column_exists = 0) then
       execute immediate 'alter table stn.cev_non_intercompany_data add (ACCOUNT_CD VARCHAR2(20))';
+      commit;
+  end if;
+
+  -- add column to cev_non_intercompany_data
+  Select count(*) into v_column_exists
+    from ALL_TAB_COLUMNS
+    where upper(column_name) = 'JL_DESCRIPTION'
+      and upper(table_name) = 'CEV_NON_INTERCOMPANY_DATA'
+      and owner = 'STN' ;
+
+  if (v_column_exists = 0) then
+      execute immediate 'alter table stn.cev_non_intercompany_data add (JL_DESCRIPTION VARCHAR2(80))';
       commit;
   end if;
 
@@ -157,6 +205,17 @@ BEGIN
   end if;        
 
 
+  -- add column to cev_valid
+  Select count(*) into v_column_exists
+    from ALL_TAB_COLUMNS
+    where upper(column_name) = 'JL_DESCRIPTION'
+      and upper(table_name) = 'CEV_VALID'
+      and owner = 'STN' ;
+
+  if (v_column_exists = 0) then
+      execute immediate 'alter table stn.cev_valid add (JL_DESCRIPTION VARCHAR2(80))';
+      commit;
+  end if;
 
 -- add column to cev_mtm_data
   Select count(*) into v_column_exists
@@ -179,6 +238,18 @@ BEGIN
 
   if (v_column_exists = 0) then
       execute immediate 'alter table stn.cev_mtm_data add (CHARTFIELD_1 VARCHAR2(50))';
+      commit;
+  end if;        
+
+-- add column to cev_mtm_data
+  Select count(*) into v_column_exists
+    from ALL_TAB_COLUMNS
+    where upper(column_name) = 'JL_DESCRIPTION'
+      and upper(table_name) = 'CEV_MTM_DATA'
+      and owner = 'STN' ;
+
+  if (v_column_exists = 0) then
+      execute immediate 'alter table stn.cev_mtm_data add (JL_DESCRIPTION VARCHAR2(80))';
       commit;
   end if;        
 
@@ -206,6 +277,18 @@ BEGIN
       commit;
   end if;        
 
+-- add column to cev_gaap_fut_accts_data
+  Select count(*) into v_column_exists
+    from ALL_TAB_COLUMNS
+    where upper(column_name) = 'JL_DESCRIPTION'
+      and upper(table_name) = 'CEV_GAAP_FUT_ACCTS_DATA'
+      and owner = 'STN' ;
+
+  if (v_column_exists = 0) then
+      execute immediate 'alter table stn.cev_gaap_fut_accts_data add (JL_DESCRIPTION VARCHAR2(80))';
+      commit;
+  end if;        
+
 -- add column to cev_data
   Select count(*) into v_column_exists
     from ALL_TAB_COLUMNS
@@ -229,6 +312,19 @@ BEGIN
       execute immediate 'alter table stn.cev_data add (CHARTFIELD_1 VARCHAR2(20))';
       commit;
   end if;        
+
+-- add column to cev_data
+  Select count(*) into v_column_exists
+    from ALL_TAB_COLUMNS
+    where upper(column_name) = 'JL_DESCRIPTION'
+      and upper(table_name) = 'CEV_DATA'
+      and owner = 'STN' ;
+
+  if (v_column_exists = 0) then
+      execute immediate 'alter table stn.cev_data add (JL_DESCRIPTION VARCHAR2(80))';
+      commit;
+  end if;        
+
 
 END;
 /
