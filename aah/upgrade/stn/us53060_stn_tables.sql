4078caa695fd9f5d2da3351c5444d548dbbc3d03
Berner, John <JBerner@agltd.com>
2021-11-03 19:42:35 +0000

    Updated us53060_stn_tables.sql

--- aah/upgrade/stn/us53060_stn_tables.sql
+++ aah/upgrade/stn/us53060_stn_tables.sql
@@ -4,3 +4,7 @@
 @@../aahCustom/aahStandardisation/src/main/db/tables/stn/cev_vie_data.sql
 @@../aahCustom/aahStandardisation/src/main/db/tables/stn/cev_intercompany_data.sql
 @@../aahCustom/aahStandardisation/src/main/db/tables/stn/cev_non_intercompany_data.sql
+@@../aahCustom/aahStandardisation/src/main/db/tables/stn/cev_data.sql
+@@../aahCustom/aahStandardisation/src/main/db/tables/stn/cev_valid.sql
+@@../aahCustom/aahStandardisation/src/main/db/tables/stn/cev_mtm_data.sql
+@@../aahCustom/aahStandardisation/src/main/db/tables/stn/cev_gaap_fut_accts_data.sql
