08c22e7fa9f8abfad98f00d2293acc66e28197c5
John Berner <JBerner@agltd.com>
2022-03-03 19:49:19 +0000

    Updated us53060_stn_tables.sql

--- aah/upgrade/stn/us53060_stn_tables.sql
+++ aah/upgrade/stn/us53060_stn_tables.sql
@@ -1,6 +1,3 @@
-@@../aahCustom/aahStandardisation/src/main/db/tables/stn/load_fr_account_lookup.sql
-@@../aahCustom/aahStandardisation/src/main/db/tables/stn/posting_account_derivation.sql
-@@../aahCustom/aahStandardisation/src/main/db/tables/stn/vie_posting_account_derivation.sql
 @@../aahCustom/aahStandardisation/src/main/db/tables/stn/cev_vie_data.sql
 @@../aahCustom/aahStandardisation/src/main/db/tables/stn/cev_intercompany_data.sql
 @@../aahCustom/aahStandardisation/src/main/db/tables/stn/cev_non_intercompany_data.sql
