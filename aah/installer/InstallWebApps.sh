66a1dec4abc258b39ec7f4c2f5c1a115f21727ce
Ryan Quigley <rquigley@assuredguaranty.com>
2022-05-02 14:38:22 -0400

    fix error messages

--- aah/installer/InstallWebApps.sh
+++ aah/installer/InstallWebApps.sh
@@ -15,19 +15,19 @@ ERR_EXIT () {
 printf "*** $PROGRAM starts ... $(date +'%F %T')\n"
 
 printf "Running installer for setupDatabaseSchemas\n"
-cp ./ApplicationResources.properties ./aah-web-setup/assets || ERR_EXIT "EROR coping application resources property file"
-./run.sh unattended -rf ${AahInstallerYaml} -op installWebApps || ERR_EXIT "ERROR running aah insaller for web apps"
+cp ./ApplicationResources.properties ./aah-web-setup/assets || ERR_EXIT "EROR coping application resources property file\n"
+./run.sh unattended -rf ${AahInstallerYaml} -op installWebApps || ERR_EXIT "ERROR running aah insaller for web apps\n"
 
 printf "setup security-external-api\n"
 
 mkdir -p ${SecurityApiDirectory} || ERR_EXIT "ERROR creating security api directory"
 
-unzip ./security-external-api/zip/security-api-service.zip -o -d ${SecurityApiDirectory} || ERR_EXIT "ERROR unzipping security api"
-cp ./drivers/*.jar ${SecurityApiDirectory}/drivers || ERR_EXIT "ERROR copy driver Jar files"
-cp ./application.properties ${SecurityApiDirectory}/config || ERR_EXIT "ERROR copying application.properties file"
+unzip ./security-external-api/zip/security-api-service.zip -o -d ${SecurityApiDirectory} || ERR_EXIT "ERROR unzipping security api\n"
+cp ./drivers/*.jar ${SecurityApiDirectory}/drivers || ERR_EXIT "ERROR copy driver Jar files\n"
+cp ./application.properties ${SecurityApiDirectory}/config || ERR_EXIT "ERROR copying application.properties file\n"
 
 
-printf "***** running security api ******"
+printf "***** running security api ******\n"
 nohup ${AahInstallerYaml}/bin/security-external.sh &
 
 
