85f6579327def8ea0a24c91abf9ee7f65272fa4f
Ryan Quigley <rquigley@assuredguaranty.com>
2022-05-04 22:57:23 -0400

    comment out check upgrade

--- aah/upgrade/install.sql
+++ aah/upgrade/install.sql
@@ -22,8 +22,9 @@ define unittest_login=~8
 
 /* Check AAH upgrade versions - do not remove */
 
-conn ~sys_logon
-@@sys/00001_check_upgrade_versions.sql
+--comment out for upgrade as this is not repeatable
+--conn ~sys_logon
+--@@sys/00001_check_upgrade_versions.sql
 
 /* Begin AAH custom upgrades */
 
