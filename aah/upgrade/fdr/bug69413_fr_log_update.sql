8e9977bfae1f3cfcb7727c5ef3d6afcd50d23cf4
John Berner <JBerner@agltd.com>
2022-05-16 14:09:23 +0000

    Added bug69413_fr_log_update.sql to /aah/upgrade/fdr

---
+++ aah/upgrade/fdr/bug69413_fr_log_update.sql
@@ -0,0 +1,6 @@
+update fdr.fr_log 
+set LO_TABLE_IN_ERROR_NAME = 'cession_event'
+where trunc(LO_EVENT_DATETIME) = '11-MAY-22'
+ AND LO_TABLE_IN_ERROR_NAME = 'gl_account'
+ AND LO_FIELD_IN_ERROR_NAME = 'acct_cd'
+ AND LO_ERROR_STATUS = 'E';
