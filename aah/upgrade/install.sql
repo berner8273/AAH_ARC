14be1309416ae463f370f99365fd473374ec3717
John Berner <JBerner@agltd.com>
2022-06-30 19:57:57 +0000

    Updated install.sql

--- aah/upgrade/install.sql
+++ aah/upgrade/install.sql
@@ -31,7 +31,7 @@ define unittest_login=~8
 conn ~fdr_logon
 -- update control file
 update fdr.fr_archive_ctl
-set arct_archive = 'Y',arct_archive_days=182, arct_archive_group = 3
+set arct_archive = 'Y',arct_archive_days=182, arct_archive_group = 3, arct_generate_script_only = 'N'
 where arct_id in (132,135,254,255,257,261,263);
 commit;
 
