76458864622fe59dc333f46993c08bacb88cc792
John Berner <JBerner@agltd.com>
2022-11-04 19:14:06 +0000

    Updated fr_general_codes.sql

--- aah/aahCustom/aahStandardisation/src/main/db/grants/tables/fdr/fr_general_codes.sql
+++ aah/aahCustom/aahStandardisation/src/main/db/grants/tables/fdr/fr_general_codes.sql
@@ -1,3 +1,4 @@
 grant select on fdr.fr_general_codes to stn;
 grant update on fdr.fr_general_codes to stn;
 grant select on fdr.fr_general_codes to gui with grant option;
+grant select on fdr.fr_general_codes to stn with grant option;
