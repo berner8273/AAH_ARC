b78838cc82de84506c4da79ce4f5cd8be042ec2b
John Berner <JBerner@agltd.com>
2023-03-15 15:57:24 +0000

    Updated fak_balances.sql

--- aah/utilities/fak_balances.sql
+++ aah/utilities/fak_balances.sql
@@ -1,3 +1,5 @@
+-- by balance date
+
 select fdb_balance_date,FDB_BALANCE_TYPE,fdb_entity,sum(fdb_tran_ltd_balance),sum(fdb_base_ltd_balance),sum(fdb_local_ltd_balance) from slr.slr_fak_daily_balances
 where FDB_BALANCE_TYPE||fdb_fak_id||fdb_balance_date in ( 
 WITH maxbal
@@ -10,3 +12,20 @@ as (
         from slr.slr_fak_daily_balances db
         join maxbal m on db.fdb_fak_id = m.fdb_fak_id and db.fdb_balance_date = m.mdate)
 group by fdb_balance_date,FDB_BALANCE_TYPE,fdb_entity
+
+-- by entity
+
+select FDB_BALANCE_TYPE,fdb_entity,sum(fdb_tran_ltd_balance),sum(fdb_base_ltd_balance),sum(fdb_local_ltd_balance) from slr.slr_fak_daily_balances
+where FDB_BALANCE_TYPE||fdb_fak_id||fdb_balance_date in ( 
+WITH maxbal
+as (
+    select fdb_fak_id,FDB_BALANCE_TYPE,max(fdb_balance_date) mdate 
+        from slr.slr_fak_daily_balances
+        group by fdb_fak_id,FDB_BALANCE_TYPE
+        )
+    select db.FDB_BALANCE_TYPE||db.fdb_fak_id||db.fdb_balance_date
+        from slr.slr_fak_daily_balances db
+        join maxbal m on db.fdb_fak_id = m.fdb_fak_id and db.fdb_balance_date = m.mdate)
+group by FDB_BALANCE_TYPE,fdb_entity
+
+
