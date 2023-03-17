dfc9234a9b8538171193a8b1a210d78c20a6e3fa
John Berner <JBerner@agltd.com>
2023-03-17 15:12:02 +0000

    Updated fak_balances.sql

--- aah/utilities/fak_balances.sql
+++ aah/utilities/fak_balances.sql
@@ -10,7 +10,8 @@ as (
         )
     select db.FDB_BALANCE_TYPE||db.fdb_fak_id||db.fdb_balance_date
         from slr.slr_fak_daily_balances db
-        join maxbal m on db.fdb_fak_id = m.fdb_fak_id and db.fdb_balance_date = m.mdate)
+        join maxbal m on db.fdb_fak_id = m.fdb_fak_id 
+        and db.fdb_balance_type= m.fdb_balance_type and db.fdb_balance_date = m.mdate)
 group by fdb_balance_date,FDB_BALANCE_TYPE,fdb_entity
 
 -- by entity
@@ -25,7 +26,8 @@ as (
         )
     select db.FDB_BALANCE_TYPE||db.fdb_fak_id||db.fdb_balance_date
         from slr.slr_fak_daily_balances db
-        join maxbal m on db.fdb_fak_id = m.fdb_fak_id and db.fdb_balance_date = m.mdate)
+        join maxbal m on db.fdb_fak_id = m.fdb_fak_id and db.fdb_balance_type= m.fdb_balance_type 
+        and db.fdb_balance_date = m.mdate)
 group by FDB_BALANCE_TYPE,fdb_entity
 
 
