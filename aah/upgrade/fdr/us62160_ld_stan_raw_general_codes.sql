0ad85deeb08fbedb3fa90d7f99cebeccf41924bf
John Berner <JBerner@agltd.com>
2023-05-02 15:36:59 +0000

    Updated us62160_ld_stan_raw_general_codes.sql

--- aah/upgrade/fdr/us62160_ld_stan_raw_general_codes.sql
+++ aah/upgrade/fdr/us62160_ld_stan_raw_general_codes.sql
@@ -1,1 +1,1 @@
-@@../aahCustom/aahStandardisation/src/main/db/procedures/fdr/PR_STAN_RAW_GENERAL_CODES_LD.prc;
+@@../aahCustom/aahStandardisation/src/main/db/procedures/fdr/pr_stan_raw_general_codes_ld.prc;
