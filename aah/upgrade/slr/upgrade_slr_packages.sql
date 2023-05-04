45d72637322b0449ca3e6dac9b182bd96d8bf737
John Berner <JBerner@agltd.com>
2023-05-04 15:10:23 +0000

    Updated upgrade_slr_packages.sql

--- aah/upgrade/slr/upgrade_slr_packages.sql
+++ aah/upgrade/slr/upgrade_slr_packages.sql
@@ -1,8 +1,8 @@
-@@../aahCustom/aahSubLedger/src/main/db/packages/slr/slr_balance_movement_pkg.hdr;
-@@../aahCustom/aahSubLedger/src/main/db/packages/slr/slr_balance_movement_pkg.bdy;
-@@../aahCustom/aahSubLedger/src/main/db/packages/slr/slr_post_journals_pkg.hdr;
-@@../aahCustom/aahSubLedger/src/main/db/packages/slr/slr_post_journals_pkg.bdy;
-@@../aahCustom/aahSubLedger/src/main/db/packages/slr/slr_validate_journals_pkg.hdr;
-@@../aahCustom/aahSubLedger/src/main/db/packages/slr/slr_validate_journals_pkg.bdy;
 @@../aahCustom/aahSubLedger/src/main/db/packages/slr/slr_utilities_pkg.hdr;
 @@../aahCustom/aahSubLedger/src/main/db/packages/slr/slr_utilities_pkg.bdy;
+@@../aahCustom/aahSubLedger/src/main/db/packages/slr/slr_validate_journals_pkg.hdr;
+@@../aahCustom/aahSubLedger/src/main/db/packages/slr/slr_validate_journals_pkg.bdy;
+@@../aahCustom/aahSubLedger/src/main/db/packages/slr/slr_post_journals_pkg.hdr;
+@@../aahCustom/aahSubLedger/src/main/db/packages/slr/slr_post_journals_pkg.bdy;
+@@../aahCustom/aahSubLedger/src/main/db/packages/slr/slr_balance_movement_pkg.hdr;
+@@../aahCustom/aahSubLedger/src/main/db/packages/slr/slr_balance_movement_pkg.bdy;
