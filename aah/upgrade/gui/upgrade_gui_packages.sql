e7e07ac1db861d0729d16ce1275b5befea24cc16
John Berner <JBerner@agltd.com>
2023-05-05 15:31:45 +0000

    Updated upgrade_gui_packages.sql

--- aah/upgrade/gui/upgrade_gui_packages.sql
+++ aah/upgrade/gui/upgrade_gui_packages.sql
@@ -1,7 +1,11 @@
 @@../aahCustom/aahSubLedger/src/main/db/packages/gui/pgui_manual_journal.hdr
 @@../aahCustom/aahSubLedger/src/main/db/packages/gui/pgui_manual_journal.bdy
 
-grant select on gui.gui_jrnl_line_errors to rdr with grant option;
-grant select on gui.gui_jrnl_lines_unposted to rdr with grant option;
-grant select on gui.gui_jrnl_headers_unposted to rdr with grant option;
-grant select on gui.gui_jrnl_headers_unposted  to rdr with grant option;
+grant select on gui.temp_gui_jrnl_lines_unposted to rdr;
+grant insert on gui.temp_gui_jrnl_lines_unposted to rdr;
+grant update on gui.temp_gui_jrnl_lines_unposted to rdr;
+grant select on gui.gui_jrnl_headers_unposted to rdr;
+grant select on gui.gui_jrnl_line_errors to rdr;
+grant select on gui.gui_jrnl_headers_unposted to slr;
+grant insert on gui.gui_jrnl_headers_unposted to slr;
+grant update on gui.gui_jrnl_headers_unposted to slr;
