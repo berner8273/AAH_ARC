a4741703888474f13c30cb9c0eed5da7543fc26e
John Berner <JBerner@agltd.com>
2023-05-05 19:04:12 +0000

    Updated upgrade_gui_packages.sql

--- aah/upgrade/gui/upgrade_gui_packages.sql
+++ aah/upgrade/gui/upgrade_gui_packages.sql
@@ -1,5 +1,5 @@
-@@../aahCustom/aahSubLedger/src/main/db/packages/gui/pgui_manual_journal.hdr;
-@@../aahCustom/aahSubLedger/src/main/db/packages/gui/pgui_manual_journal.bdy;
+@@../aahCustom/aahSubLedger/src/main/db/packages/gui/PGUI_MANUAL_JOURNAL.hdr;
+@@../aahCustom/aahSubLedger/src/main/db/packages/gui/PGUI_MANUAL_JOURNAL.bdy;
 
 
 grant select on gui.temp_gui_jrnl_lines_unposted to rdr;
