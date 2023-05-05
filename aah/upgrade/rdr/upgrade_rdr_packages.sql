3bd640afc712e3d1fec630aa70e76f9b0894d2b7
John Berner <JBerner@agltd.com>
2023-05-05 19:08:37 +0000

    Updated upgrade_rdr_packages.sql

--- aah/upgrade/rdr/upgrade_rdr_packages.sql
+++ aah/upgrade/rdr/upgrade_rdr_packages.sql
@@ -1,12 +1,9 @@
 grant select on rdr.rrv_ag_slr_jrnl_lines to aah_rdr;
 grant select on rdr.rr_glint_journal_line to aah_glint;        
 grant update on rdr.rr_glint_journal_line to aah_glint;
-grant select on rdr.rrv_ag_glint_jrnl_lines to  aah_rdr;
+grant select on rdr.rrv_ag_glint_jrnl_lines to aah_rdr;
 grant select on rdr.rrv_ag_stan_raw_acc_event to aah_rdr;
--- rrv_ag_slr_jrnl_headers -- removed extra hash
--- rrv_ag_slr_jrnl_lines -- removed extra hash
-
 @@../aahCustom/aahSubLedger/src/main/db/packages/rdr/PG_GLINT.hdr;
 @@../aahCustom/aahSubLedger/src/main/db/packages/rdr/PG_GLINT.bdy;
-@@../aahCustom/aahRDR/src/main/db/views/rdr/rrv_ag_slr_jrnl_headers.sql
-@@../aahCustom/aahRDR/src/main/db/views/rdr/rrv_ag_slr_jrnl_lines.sql
+@@../aahCustom/aahRDR/src/main/db/views/rdr/rrv_ag_slr_jrnl_headers.sql;
+@@../aahCustom/aahRDR/src/main/db/views/rdr/rrv_ag_slr_jrnl_lines.sql;
