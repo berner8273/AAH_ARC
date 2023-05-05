6948ae8fe5874105d7efb4f221ee395d95c0b9f4
John Berner <JBerner@agltd.com>
2023-05-05 15:16:38 +0000

    Updated upgrade_rdr_packages.sql

--- aah/upgrade/rdr/upgrade_rdr_packages.sql
+++ aah/upgrade/rdr/upgrade_rdr_packages.sql
@@ -1,4 +1,8 @@
-grant select on rdr.rrv_ag_slr_jrnl_lines to aah_rdr;
+
+grant  select on rdr.rr_glint_journal_line to aah_glint;        
+grant  update on rdr.rr_glint_journal_line to aah_glint;
+grant select on rdr.rrv_ag_glint_jrnl_lines to  aah_rdr;
+grant select on rdr.rrv_ag_stan_raw_acc_event to aah_rdr;
 
 @@../aahCustom/aahSubLedger/src/main/db/packages/rdr/PG_GLINT.hdr;
 @@../aahCustom/aahSubLedger/src/main/db/packages/rdr/PG_GLINT.bdy;
