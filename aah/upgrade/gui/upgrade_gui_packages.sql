52c88f1349226f78e998d6464e63fd7087190c58
John Berner <JBerner@agltd.com>
2023-05-05 15:41:14 +0000

    Updated upgrade_gui_packages.sql

--- aah/upgrade/gui/upgrade_gui_packages.sql
+++ aah/upgrade/gui/upgrade_gui_packages.sql
@@ -1,6 +1,7 @@
 @@../aahCustom/aahSubLedger/src/main/db/packages/gui/pgui_manual_journal.hdr;
 @@../aahCustom/aahSubLedger/src/main/db/packages/gui/pgui_manual_journal.bdy;
 
+
 grant select on gui.temp_gui_jrnl_lines_unposted to rdr;
 grant insert on gui.temp_gui_jrnl_lines_unposted to rdr;
 grant update on gui.temp_gui_jrnl_lines_unposted to rdr;
@@ -9,6 +10,12 @@ grant select on gui.gui_jrnl_line_errors to rdr;
 grant select on gui.gui_jrnl_headers_unposted to slr;
 grant insert on gui.gui_jrnl_headers_unposted to slr;
 grant update on gui.gui_jrnl_headers_unposted to slr;
+
+grant select on gui.gui_jrnl_line_errors to rdr with grant option
+grant select on gui.gui_jrnl_lines_unposted to rdr with grant option
+grant select on gui.gui_jrnl_headers_unposted to rdr with grant option
+grant select on gui.gui_jrnl_headers_unposted  to rdr with grant option
+grant select on rdr.rrv_ag_slr_jrnl_lines to aah_rdr;
 
 update gui.t_ui_jrnl_line_meta
 set column_nullable = 'Y'
