10e04ddc57fa0f35b2831ee95b0815e944bc17cb
John Berner <JBerner@agltd.com>
2023-05-10 19:17:10 +0000

    Updated upgrade_slr_packages.sql

--- aah/upgrade/slr/upgrade_slr_packages.sql
+++ aah/upgrade/slr/upgrade_slr_packages.sql
@@ -4,5 +4,5 @@
 @@../aahCustom/aahSubLedger/src/main/db/packages/slr/SLR_VALIDATE_JOURNALS_PKG.bdy;
 @@../aahCustom/aahSubLedger/src/main/db/packages/slr/SLR_POST_JOURNALS_PKG.hdr;
 @@../aahCustom/aahSubLedger/src/main/db/packages/slr/SLR_POST_JOURNALS_PKG.bdy;
-@@../aahCustom/aahSubLedger/src/main/db/packages/slr/SLR_BALANCE_MOVEMENT_pkg.hdr;
-@@../aahCustom/aahSubLedger/src/main/db/packages/slr/SLR_BALANCE_MOVEMENT_pkg.bdy;
+@@../aahCustom/aahSubLedger/src/main/db/packages/slr/SLR_BALANCE_MOVEMENT_PKG.hdr;
+@@../aahCustom/aahSubLedger/src/main/db/packages/slr/SLR_BALANCE_MOVEMENT_PKG.bdy;
