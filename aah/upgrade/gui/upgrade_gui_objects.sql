2c1dda44382f9dd59c77fa22037a1177cef878e7
John Berner <JBerner@agltd.com>
2023-05-10 19:37:14 +0000

    Updated upgrade_gui_objects.sql

--- aah/upgrade/gui/upgrade_gui_objects.sql
+++ aah/upgrade/gui/upgrade_gui_objects.sql
@@ -10,11 +10,10 @@ grant select on gui.gui_jrnl_line_errors to rdr;
 grant select on gui.gui_jrnl_headers_unposted to slr;
 grant insert on gui.gui_jrnl_headers_unposted to slr;
 grant update on gui.gui_jrnl_headers_unposted to slr;
-
-grant select on gui.gui_jrnl_line_errors to rdr with grant option
-grant select on gui.gui_jrnl_lines_unposted to rdr with grant option
-grant select on gui.gui_jrnl_headers_unposted to rdr with grant option
-grant select on gui.gui_jrnl_headers_unposted  to rdr with grant option
+grant select on gui.gui_jrnl_line_errors to rdr with grant option;
+grant select on gui.gui_jrnl_lines_unposted to rdr with grant option;
+grant select on gui.gui_jrnl_headers_unposted to rdr with grant option;
+grant select on gui.gui_jrnl_headers_unposted  to rdr with grant option;
 
 update gui.t_ui_jrnl_line_meta
 set column_nullable = 'Y'
