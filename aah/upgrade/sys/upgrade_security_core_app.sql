89504103188120b7c3f331e8d6b06ea5d7feb732
John Berner <JBerner@agltd.com>
2023-05-30 20:41:03 +0000

    Updated upgrade_security_core_app.sql

--- aah/upgrade/sys/upgrade_security_core_app.sql
+++ aah/upgrade/sys/upgrade_security_core_app.sql
@@ -1,30 +1,30 @@
 update security_core.application 
 set base_url =
     case sys_context('userenv', 'db_name')
-        when 'APTDEV' then 'http://aptitudedev.agl.com/aah'
-        when 'APTQA'  then 'http://aptitudedqa.agl.com/aah'       
-        when 'APTUAT' then 'http://aptitudeduat.agl.com/aah'
-        when 'APT'    then 'http://aptitude.agl.com/aah'
+        when 'APTDEV' then 'https://aptitudedev.agl.com/aah'
+        when 'APTQA'  then 'https://aptitudedqa.agl.com/aah'       
+        when 'APTUAT' then 'https://aptitudeduat.agl.com/aah'
+        when 'APT'    then 'https://aptitude.agl.com/aah'
         end
 where application_name = 'AAH';
 
 update security_core.application 
 set base_url =
     case sys_context('userenv', 'db_name')
-        when 'APTDEV' then 'http://aptitudedev.agl.com/scheduler-web'
-        when 'APTQA'  then 'http://aptitudedqa.agl.com/scheduler-web'       
-        when 'APTUAT' then 'http://aptitudeduat.agl.com/scheduler-web'
-        when 'APT'    then 'http://aptitude.agl.com/scheduler-web'
+        when 'APTDEV' then 'https://aptitudedev.agl.com/scheduler-web'
+        when 'APTQA'  then 'https://aptitudedqa.agl.com/scheduler-web'       
+        when 'APTUAT' then 'https://aptitudeduat.agl.com/scheduler-web'
+        when 'APT'    then 'https://aptitude.agl.com/scheduler-web'
         end
 where application_name = 'ASCHED';
 
 update security_core.application 
 set base_url =
     case sys_context('userenv', 'db_name')
-        when 'APTDEV' then 'http://aptitudedev.agl.com/SECURITY'
-        when 'APTQA'  then 'http://aptitudedqa.agl.com/SECURITY'       
-        when 'APTUAT' then 'http://aptitudeduat.agl.com/SECURITY'
-        when 'APT'    then 'http://aptitude.agl.com/SECURITY'
+        when 'APTDEV' then 'https://aptitudedev.agl.com/SECURITY'
+        when 'APTQA'  then 'https://aptitudedqa.agl.com/SECURITY'       
+        when 'APTUAT' then 'https://aptitudeduat.agl.com/SECURITY'
+        when 'APT'    then 'https://aptitude.agl.com/SECURITY'
         end
 where application_name = 'ASEC';
 
