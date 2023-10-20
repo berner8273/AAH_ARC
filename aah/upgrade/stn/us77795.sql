36be70f1369cac2df1ec69a8f571b7529cba3401
John Berner <JBerner@agltd.com>
2023-10-20 20:55:20 +0000

    Updated us77795.sql

--- aah/upgrade/stn/us77795.sql
+++ aah/upgrade/stn/us77795.sql
@@ -1,6 +1,13 @@
+delete from stn.step_run_state where step_run_sid in (
+select step_run_sid from stn.step_run where step_id = 56);
+delete from stn.step_run_status where step_run_status_id in (
+select step_run_status_id from stn.step_run_state where step_run_sid in (
+select step_run_sid from stn.step_run where step_id = 56));
+delete from stn.step_run_param where step_run_sid in (
+select step_run_sid from stn.step_run where step_id = 56);
+delete from stn.step_run where step_id = 56;
 delete from stn.step where step_id=56;
 delete from stn.process where process_id=53;
-
 
 Insert into stn.PROCESS
    (PROCESS_ID, PROJECT_ID, PROCESS_NAME, PROCESS_TYPE_ID, INPUT_NODE_NAME)
@@ -11,4 +18,4 @@ Insert into stn.STEP
    (STEP_ID, STEP_CD, PROCESS_ID, PARAM_SET_ID)
  Values
    (56, 'pDeleteSLRPartitions', 53, 6);
-   commit;
+commit;
