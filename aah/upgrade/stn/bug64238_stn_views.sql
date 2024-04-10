14b3cecc99f0e43218e9d78f0a5b1d3bc6906914
John Berner <JBerner@agltd.com>
2024-04-10 19:13:40 +0000

    Updated bug64238_stn_views.sql

--- aah/upgrade/stn/bug64238_stn_views.sql
+++ aah/upgrade/stn/bug64238_stn_views.sql
@@ -1,2 +1,1 @@
-@@../aahCustom/aahStandardisation/src/main/db/views/stn/cession_event_reversal_hist.sql
-@@../aahCustom/aahStandardisation/src/main/db/views/stn/cession_event_reversal_curr.sql
+@@../aahCustom/aahStandardisation/src/main/db/views/stn/cession_event_hopper.sql
