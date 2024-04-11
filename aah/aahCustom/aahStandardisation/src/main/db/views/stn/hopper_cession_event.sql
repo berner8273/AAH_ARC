5f4a39e29551f25a9306c80d1bfe9fe6ed980235
John Berner <JBerner@agltd.com>
2024-04-11 16:59:28 +0000

    Updated hopper_cession_event.sql

--- aah/aahCustom/aahStandardisation/src/main/db/views/stn/hopper_cession_event.sql
+++ aah/aahCustom/aahStandardisation/src/main/db/views/stn/hopper_cession_event.sql
@@ -1,61 +1,124 @@
-create or replace view stn.hopper_cession_event
-as
-select
-       fsrae.srae_gl_entity              business_unit
-     , fsrae.srae_gl_book                book_cd
-     , fsrae.srae_dimension_1            chartfield_1
-     , fsrae.srae_dimension_2            dept_cd
-     , fsrae.srae_dimension_3            counterparty_le_cd
-     , fsrae.srae_dimension_4            affiliate_le_cd
-     , fsrae.srae_dimension_5            accident_yr
-     , fsrae.srae_dimension_6            underwriting_yr
-     , fsrae.srae_dimension_7            policy_id
-     , fsrae.srae_dimension_8            stream_id
-     , fsrae.srae_dimension_9            tax_jurisdiction_cd
-     , fsrae.srae_dimension_10           ledger_cd
-     , fsrae.srae_dimension_11           execution_typ
-     , fsrae.srae_dimension_12           business_typ
-     , fsrae.srae_dimension_13           owner_le_cd
-     , fsrae.srae_dimension_14           premium_typ
-     , fsrae.srae_dimension_15           journal_descr
-     , fsrae.srae_client_spare_id2       account_cd
-     , fsrae.srae_client_spare_id3       ultimate_parent_le_cd
-     , fsrae.srae_client_spare_id4       event_typ
-     , fsrae.srae_client_spare_id5       functional_ccy
-     , fsrae.srae_client_spare_id6       functional_amt
-     , fsrae.srae_client_spare_id7       reporting_ccy
-     , fsrae.srae_client_spare_id8       reporting_amt
-     , fsrae.srae_client_spare_id9       is_mark_to_market
-     , fsrae.srae_client_spare_id10      vie_cd
-     , fsrae.srae_client_spare_id11      business_event_typ
-     , fsrae.srae_client_spare_id12      event_seq_id
-     , fsrae.srae_client_spare_id13      policy_typ
-     , fsrae.srae_client_spare_id14      correlation_uuid
-     , fsrae.srae_client_spare_id15      basis_cd
-     , fsrae.srae_client_spare_id16      posting_indicator
-     , fsrae.srae_client_spare_id17      bu_account_lookup
-     , fsrae.srae_client_spare_id18      vie_bu_account_lookup
-     , fsrae.srae_acc_event_type         aah_event_typ
-     , fsrae.srae_iso_currency_code      transaction_ccy
-     , fsrae.srae_client_amount1         transaction_amt
-     , fsrae.srae_pos_neg_flag           transaction_pos_neg
-     , fsrae.srae_sub_event_id           sub_event
-     , fsrae.srae_value_date          accounting_dt
-     , fsrae.srae_gl_party_business_code party_business_le_cd
-     , fsrae.srae_party_sys_inst_code    party_business_system_cd
-     , fsrae.srae_instr_super_class      srae_instr_super_class
-     , fsrae.srae_static_sys_inst_code   srae_static_sys_inst_code
-     , fsrae.srae_instr_sys_inst_code    srae_instr_sys_inst_code
-     , fsrae.srae_gl_person_code         srae_gl_person_code
-     , fsrae.srae_source_system          srae_source_system
-     , fsrae.srae_instrument_code        srae_instrument_code
-     , fsrae.srae_posting_date           posting_dt
-     , fsrae.srae_client_date1           effective_dt
-     , fsrae.event_status                event_status
-     , fsrae.message_id                  message_id
-     , fsrae.process_id                  process_id
-     , fsrae.lpg_id                      lpg_id
-     , fsrae.srae_client_date2           original_posting_dt
-  from
-       fdr.fr_stan_raw_acc_event fsrae
-     ;
+DROP VIEW STN.HOPPER_CESSION_EVENT;
+
+/* Formatted on 04/11/2024 12:51:42 PM (QP5 v5.252.13127.32847) */
+CREATE OR REPLACE FORCE VIEW STN.HOPPER_CESSION_EVENT
+(
+   BUSINESS_UNIT,
+   BOOK_CD,
+   CHARTFIELD_1,
+   DEPT_CD,
+   COUNTERPARTY_LE_CD,
+   AFFILIATE_LE_CD,
+   ACCIDENT_YR,
+   UNDERWRITING_YR,
+   POLICY_ID,
+   STREAM_ID,
+   TAX_JURISDICTION_CD,
+   LEDGER_CD,
+   EXECUTION_TYP,
+   BUSINESS_TYP,
+   OWNER_LE_CD,
+   PREMIUM_TYP,
+   JOURNAL_DESCR,
+   ACCOUNT_CD,
+   ULTIMATE_PARENT_LE_CD,
+   EVENT_TYP,
+   FUNCTIONAL_CCY,
+   FUNCTIONAL_AMT,
+   REPORTING_CCY,
+   REPORTING_AMT,
+   IS_MARK_TO_MARKET,
+   VIE_CD,
+   BUSINESS_EVENT_TYP,
+   EVENT_SEQ_ID,
+   POLICY_TYP,
+   CORRELATION_UUID,
+   BASIS_CD,
+   POSTING_INDICATOR,
+   BU_ACCOUNT_LOOKUP,
+   VIE_BU_ACCOUNT_LOOKUP,
+   JL_DESCRIPTION,
+   AAH_EVENT_TYP,
+   TRANSACTION_CCY,
+   TRANSACTION_AMT,
+   TRANSACTION_POS_NEG,
+   SUB_EVENT,
+   ACCOUNTING_DT,
+   PARTY_BUSINESS_LE_CD,
+   PARTY_BUSINESS_SYSTEM_CD,
+   SRAE_INSTR_SUPER_CLASS,
+   SRAE_STATIC_SYS_INST_CODE,
+   SRAE_INSTR_SYS_INST_CODE,
+   SRAE_GL_PERSON_CODE,
+   SRAE_SOURCE_SYSTEM,
+   SRAE_INSTRUMENT_CODE,
+   POSTING_DT,
+   EFFECTIVE_DT,
+   EVENT_STATUS,
+   MESSAGE_ID,
+   PROCESS_ID,
+   LPG_ID,
+   ORIGINAL_POSTING_DT
+)
+   BEQUEATH DEFINER
+AS
+   SELECT fsrae.srae_gl_entity business_unit,
+          fsrae.srae_gl_book book_cd,
+          fsrae.srae_dimension_1 chartfield_1,
+          fsrae.srae_dimension_2 dept_cd,
+          fsrae.srae_dimension_3 counterparty_le_cd,
+          fsrae.srae_dimension_4 affiliate_le_cd,
+          fsrae.srae_dimension_5 accident_yr,
+          fsrae.srae_dimension_6 underwriting_yr,
+          fsrae.srae_dimension_7 policy_id,
+          fsrae.srae_dimension_8 stream_id,
+          fsrae.srae_dimension_9 tax_jurisdiction_cd,
+          fsrae.srae_dimension_10 ledger_cd,
+          fsrae.srae_dimension_11 execution_typ,
+          fsrae.srae_dimension_12 business_typ,
+          fsrae.srae_dimension_13 owner_le_cd,
+          fsrae.srae_dimension_14 premium_typ,
+          fsrae.srae_dimension_15 journal_descr,
+          fsrae.srae_client_spare_id2 account_cd,
+          fsrae.srae_client_spare_id3 ultimate_parent_le_cd,
+          fsrae.srae_client_spare_id4 event_typ,
+          fsrae.srae_client_spare_id5 functional_ccy,
+          fsrae.srae_client_spare_id6 functional_amt,
+          fsrae.srae_client_spare_id7 reporting_ccy,
+          fsrae.srae_client_spare_id8 reporting_amt,
+          fsrae.srae_client_spare_id9 is_mark_to_market,
+          fsrae.srae_client_spare_id10 vie_cd,
+          fsrae.srae_client_spare_id11 business_event_typ,
+          fsrae.srae_client_spare_id12 event_seq_id,
+          fsrae.srae_client_spare_id13 policy_typ,
+          fsrae.srae_client_spare_id14 correlation_uuid,
+          fsrae.srae_client_spare_id15 basis_cd,
+          fsrae.srae_client_spare_id16 posting_indicator,
+          fsrae.srae_client_spare_id17 bu_account_lookup,
+          fsrae.srae_client_spare_id18 vie_bu_account_lookup,
+          fsrae.srae_client_spare_id19 jl_description,
+          fsrae.srae_acc_event_type aah_event_typ,
+          fsrae.srae_iso_currency_code transaction_ccy,
+          fsrae.srae_client_amount1 transaction_amt,
+          fsrae.srae_pos_neg_flag transaction_pos_neg,
+          fsrae.srae_sub_event_id sub_event,
+          fsrae.srae_value_date accounting_dt,
+          fsrae.srae_gl_party_business_code party_business_le_cd,
+          fsrae.srae_party_sys_inst_code party_business_system_cd,
+          fsrae.srae_instr_super_class srae_instr_super_class,
+          fsrae.srae_static_sys_inst_code srae_static_sys_inst_code,
+          fsrae.srae_instr_sys_inst_code srae_instr_sys_inst_code,
+          fsrae.srae_gl_person_code srae_gl_person_code,
+          fsrae.srae_source_system srae_source_system,
+          fsrae.srae_instrument_code srae_instrument_code,
+          fsrae.srae_posting_date posting_dt,
+          fsrae.srae_client_date1 effective_dt,
+          fsrae.event_status event_status,
+          fsrae.message_id message_id,
+          fsrae.process_id process_id,
+          fsrae.lpg_id lpg_id,
+          fsrae.srae_client_date2 original_posting_dt
+     FROM fdr.fr_stan_raw_acc_event fsrae;
+
+
+GRANT SELECT ON STN.HOPPER_CESSION_EVENT TO AAH_READ_ONLY;
