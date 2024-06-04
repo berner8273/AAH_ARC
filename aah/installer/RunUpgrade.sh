88067d8a42c700fff0b1da9730115cf73e9f0aea
John Berner <JBerner@agltd.com>
2024-06-04 14:37:15 +0000

    Updated RunUpgrade.sh

--- aah/installer/RunUpgrade.sh
+++ aah/installer/RunUpgrade.sh
@@ -97,6 +97,8 @@ cd -
 
 printf "Running installer for setupDatabaseSchemas\n"
 chmod +x ./run.sh
+chmod +rwx /aah/installer/scheduler-db/scheduler-db/migration.sh
+chmod +rwx /aah/installer/security-db/security-db/migration.sh
 ./run.sh unattended -rf ${AahInstallerYaml} -op migrateDatabaseSchemas || ERR_EXIT "ERROR running installer for migrateDatabaseSchemas\n"
 
 # printf "Run Update Security entries\n"
