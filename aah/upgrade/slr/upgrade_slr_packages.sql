240e6434b3ad79a24a82e104faa6cd95cdcd15b0
John Berner <JBerner@agltd.com>
2024-06-18 14:55:28 +0000

    Updated upgrade_slr_packages.sql

--- aah/upgrade/slr/upgrade_slr_packages.sql
+++ aah/upgrade/slr/upgrade_slr_packages.sql
@@ -8,3 +8,10 @@
 @@../aahCustom/aahSubLedger/src/main/db/packages/slr/SLR_POST_JOURNALS_PKG.bdy;
 @@../aahCustom/aahSubLedger/src/main/db/packages/slr/SLR_BALANCE_MOVEMENT_PKG.hdr;
 @@../aahCustom/aahSubLedger/src/main/db/packages/slr/SLR_BALANCE_MOVEMENT_PKG.bdy;
+
+Insert into SLR_HINTS_SETS
+   (HS_SET, HS_STATEMENT, HS_HINT, HS_DESCRIPTION)
+ Values
+   ('DEFAULT', 'MERGE_EBA_EXISTS', '/*+ no_parallel */', 'Added in 24.1.1');
+COMMIT;
+/
