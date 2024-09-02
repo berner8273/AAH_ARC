aa874cfa016ef59386c81180e7f641d068d627fa
John Berner <JBerner@agltd.com>
2024-09-02 15:14:22 +0000

    Updated install.sql

--- aah/upgrade/install.sql
+++ aah/upgrade/install.sql
@@ -24,8 +24,42 @@ define unittest_login=~8
 
 /* Begin AAH custom upgrades */
 
-conn ~stn_logon
- @@stn/us78363.sql
+conn ~slr_logon
+ DECLARE
+s_proc_name   VARCHAR2 (80) := 'slr.pr_reset_object_state'; 
+   
+BEGIN
+
+FOR r_slr IN ( select distinct process_id from slr.prc_object_state_in_process
+WHERE status in ('P','E') )
+LOOP    
+
+slr.pg_process_state.rollback_data(r_slr.process_id);
+
+fdr.pg_common.plog('pr_reset_object_state deleted: '||to_char(r_slr.process_id));
+
+END LOOP;
+
+
+EXCEPTION
+   WHEN OTHERS
+   THEN
+      fdr.pr_error (slr.slr_global_pkg.C_MAJERR,
+                'Failure to execute pr_reset_object_state: ' || SQLERRM,
+                slr.slr_global_pkg.C_TECHNICAL,
+                s_proc_name,
+                'prc_object_state_in_process',
+                NULL,
+                'SLR',
+                NULL,
+                'PL/SQL',
+                SQLCODE);
+
+      RAISE_APPLICATION_ERROR (
+         -20001,
+         'Fatal error during call of pr_reset_object_state ' || SQLERRM);
+END;
+/
  
 /* End AAH custom upgrades */
 
