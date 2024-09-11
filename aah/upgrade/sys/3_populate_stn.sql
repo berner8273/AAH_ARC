92fbbd884f6a4bc89de91ffd44f19117a2baff03
John Berner <JBerner@agltd.com>
2024-09-11 21:02:13 +0000

    Added 3_populate_stn.sql to /aah/upgrade/sys

---
+++ aah/upgrade/sys/3_populate_stn.sql
@@ -0,0 +1,15 @@
+-- move from temp table to stn.journal_line
+begin
+stn.temp_pkg_merger.pInsertJournalLine(1);
+stn.temp_pkg_merger.pInsertJournalLine(2);
+stn.temp_pkg_merger.pInsertJournalLine(3);
+stn.temp_pkg_merger.pInsertJournalLine(4);
+stn.temp_pkg_merger.pInsertJournalLine(5);
+stn.temp_pkg_merger.pInsertJournalLine(6);
+stn.temp_pkg_merger.pInsertJournalLine(7);
+stn.temp_pkg_merger.pInsertJournalLine(8);
+stn.temp_pkg_merger.pInsertJournalLine(9);
+stn.temp_pkg_merger.pInsertJournalLine(10);
+stn.temp_pkg_merger.pInsertJournalLine(11);
+commit;
+end;
