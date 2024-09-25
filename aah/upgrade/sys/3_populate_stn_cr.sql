2db99094645d3d2ecef03cffb9b1d9f599e166c4
John Berner <JBerner@agltd.com>
2024-09-25 15:38:34 +0000

    Updated 3_populate_stn_cr.sql

--- aah/upgrade/sys/3_populate_stn_cr.sql
+++ aah/upgrade/sys/3_populate_stn_cr.sql
@@ -1,16 +1,38 @@
 -- move from temp table to stn.journal_line
+declare 
+nCnt number;
+Ssql varchar(200);
+
 begin
-stn.temp_pkg_merger.pInsertJournalLine(1);
-stn.temp_pkg_merger.pInsertJournalLine(2);
-stn.temp_pkg_merger.pInsertJournalLine(3);
-stn.temp_pkg_merger.pInsertJournalLine(4);
-stn.temp_pkg_merger.pInsertJournalLine(5);
-stn.temp_pkg_merger.pInsertJournalLine(6);
-stn.temp_pkg_merger.pInsertJournalLine(7);
-stn.temp_pkg_merger.pInsertJournalLine(8);
-stn.temp_pkg_merger.pInsertJournalLine(9);
-stn.temp_pkg_merger.pInsertJournalLine(10);
-stn.temp_pkg_merger.pInsertJournalLine(11);
+
+stn.temp_pkg_merger_cr.pInsertJournalLine(1);
+stn.temp_pkg_merger_cr.pInsertJournalLine(2);
+stn.temp_pkg_merger_cr.pInsertJournalLine(3);
+stn.temp_pkg_merger_cr.pInsertJournalLine(4);
+stn.temp_pkg_merger_cr.pInsertJournalLine(5);
+stn.temp_pkg_merger_cr.pInsertJournalLine(6);
+stn.temp_pkg_merger_cr.pInsertJournalLine(7);
+stn.temp_pkg_merger_cr.pInsertJournalLine(8);
+stn.temp_pkg_merger_cr.pInsertJournalLine(9);
+stn.temp_pkg_merger_cr.pInsertJournalLine(10);
+stn.temp_pkg_merger_cr.pInsertJournalLine(11);
 commit;
+
+
+select count(*) into nCnt from all_indexes where lower(index_name) = 'ajh_temp_db';
+IF nCnt = 1 THEN
+    sSQL := 'DROP INDEX SLR.AJH_TEMP_DB';
+    Execute immediate sSQL;
+END IF;    
+
+
+
+select count(*) into nCnt from all_indexes where lower(index_name) = 'ajh_temp_slr_jl_eba_id'; 
+IF nCnt = 1 THEN
+    sSql := 'DROP INDEX SLR.AJH_TEMP_SLR_JL_EBA_ID';
+    execute immediate sSQL;
+END IF ;   
+
+
 end;
 /
