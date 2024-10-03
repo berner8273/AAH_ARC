b3b2a2bfca37cbe7d3ee2de1199ff94fc3d3a2cd
John Berner <JBerner@agltd.com>
2024-10-03 15:55:46 +0000

    Updated gui_jrnl_lines_unposted.sql

--- aah/aahCustom/aahSubLedger/src/main/db/grants/tables/gui/gui_jrnl_lines_unposted.sql
+++ aah/aahCustom/aahSubLedger/src/main/db/grants/tables/gui/gui_jrnl_lines_unposted.sql
@@ -1,3 +1,4 @@
 grant insert on gui.gui_jrnl_lines_unposted to rdr;
 grant select on gui.gui_jrnl_lines_unposted to rdr;
 grant delete on gui.gui_jrnl_lines_unposted to rdr;
+grant select on gui.gui_jrnl_lines_unposted to rdr with grant option;
