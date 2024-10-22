19cf9c15e574f7076c2f714e15a6eb934a1dd41f
John Berner <JBerner@agltd.com>
2024-10-22 18:25:37 +0000

    Updated fak_balances.sql

--- aah/utilities/fak_balances.sql
+++ aah/utilities/fak_balances.sql
@@ -1,3 +1,29 @@
+/* balance rollup row checks */
+WITH maxbal
+     AS (  SELECT fdb.fdb_fak_id,
+                  fdb.FDB_BALANCE_TYPE,
+                  MAX (fdb_balance_date) mdate
+             FROM slr.slr_fak_daily_balances fdb, fdr.fr_global_parameter g
+            WHERE     g.lpg_id = 2
+                  AND fdb_balance_date <= (g.gp_todays_bus_date - 390) + 1
+         GROUP BY fdb_fak_id, fdb.FDB_BALANCE_TYPE)
+  SELECT db.fdb_balance_type, COUNT (*) COUNT
+    FROM maxbal,
+         slr.slr_fak_daily_balances db,
+         fdr.fr_global_parameter gp,
+         fdr.fr_archive_ctl arc
+   WHERE     db.FDB_BALANCE_TYPE = maxbal.FDB_BALANCE_TYPE
+         AND db.fdb_fak_id = maxbal.fdb_fak_id
+         AND db.fdb_balance_date = maxbal.mdate
+         AND gp.lpg_id = 2
+         AND arc.arct_id = 254
+         AND maxbal.mdate <= (gp.gp_todays_bus_date - arc.arct_archive_days)
+         AND db.fdb_balance_type = maxbal.fdb_balance_type
+         AND db.fdb_balance_date <>
+                (gp.gp_todays_bus_date - arc.arct_archive_days) + 1
+         AND db.fdb_balance_type IN (20, 50)
+GROUP BY db.fdb_balance_type
+
 -- by entity
 select FDB_BALANCE_TYPE,fdb_entity,sum(fdb_tran_ltd_balance),sum(fdb_base_ltd_balance),sum(fdb_local_ltd_balance) from slr.slr_fak_daily_balances
 where FDB_BALANCE_TYPE||fdb_fak_id||fdb_balance_date in ( 
