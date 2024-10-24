53472677e7dbb4e85837a1622c57a129aaf90022
John Berner <JBerner@agltd.com>
2024-10-24 21:18:12 +0000

    Updated install.sql

--- aah/upgrade/install.sql
+++ aah/upgrade/install.sql
@@ -26,14 +26,13 @@ define unittest_login=~8
 
 conn ~fdr_logon
 -- close balance_others.  It had to be opeded to allow entries for old balances
--- close balance_others.  It had to be opeded to allow entries for old balances
 begin
 
 delete from fdr.fr_general_lookup
 where 
     lk_lkt_lookup_type_code   = 'EVENT_CLASS_PERIOD' and 
     lk_match_key1 = 'BALANCE_OTHERS' and
-    lk_match_key2 >= 2024;
+    lk_match_key2 >= '2024';
 commit;
 end;    
 /
