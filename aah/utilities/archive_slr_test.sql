9035facf1414eac7188b01c32ffb8fe3185304e1
John Berner <JBerner@agltd.com>
2024-10-30 15:39:24 +0000

    Updated archive_slr_test.sql

--- aah/utilities/archive_slr_test.sql
+++ aah/utilities/archive_slr_test.sql
@@ -21,7 +21,6 @@ DECLARE
        
 BEGIN
 
-dbms_output.put_line(q'[1,2,A,B,C,D,E,F]');
 dbms_output.put_line(q'[ID,Group,Table,Schema,Rows Need Archive,Total Rows,Archived,Archive Log]');
 
   select to_char(gp_todays_bus_date,'mm/dd/yyyy') into bus_date from slr.fr_global_parameter where lpg_id=1;
