2bb3adcaa0720714fb93b60f19ab0ba283a308f0
John Berner <JBerner@agltd.com>
2024-10-30 15:06:23 +0000

    Added parchiveSLR1.sh to /aahControl/scripts

---
+++ aahControl/scripts/parchiveSLR1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function parchiveSLR1()
+{
+
+     runStep parchiveSLR1
+
+}
+
+
+
+ERR_EXIT () {
+	printf "Error: *******************************************\n$@\n"
+	exit 1
+}
+
+function runStep()
+{
+    stepName=${1}
+    /aah/scripts/StartStep.sh ${1}
+    [[ $? = 0 ]] || ERR_EXIT ${1}
+}
+
+
+# Run all steps and print standard out and err to screen and log file
+parchiveSLR1 "$@" 2>&1 | tee parchiveSLR1$(date +"%b-%d-%y").log
+# echo "pArchiveSLR1-"${now} >>/aah/logs/pArchiveSLR1.log 
